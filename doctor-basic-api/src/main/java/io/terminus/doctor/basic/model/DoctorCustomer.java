5e1da7a882efc0b7e6616f7d56a488f4a9ca1119
DreamYoung <yangzhiliang1018@gmail.com>
2016-04-25 14:43:03 +0800

    add model

---
+++ doctor-basic-api/src/main/java/io/terminus/doctor/basic/model/DoctorCustomer.java
@@ -0,0 +1,16 @@
+package io.terminus.doctor.basic.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Desc: 猪场客户表
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 16/4/25
+ */
+@Data
+public class DoctorCustomer implements Serializable {
+    private static final long serialVersionUID = -8481923224160907584L;
+}
---
+++ doctor-user-api/src/main/java/io/terminus/doctor/user/model/DoctorResCtrlRole.java
@@ -0,0 +1,16 @@
+package io.terminus.doctor.user.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Desc: 资源角色表
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 16/4/25
+ */
+@Data
+public class DoctorResCtrlRole implements Serializable {
+    private static final long serialVersionUID = -2933024479650533302L;
+}
