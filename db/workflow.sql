1026a0cb4ce2f478a7c06fee5c97e2a153a1e73b
IceMimosa <icemimosa@IceMimosa.local>
2016-04-27 18:40:20 +0800

    db change

--- db/workflow.sql
+++ db/workflow.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `workflow_definitions`;
 CREATE TABLE IF NOT EXISTS `workflow_definitions` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
 	`key` VARCHAR(128) DEFAULT NULL COMMENT '流程定义唯一标识, 按照版本号区分',
-	`name` VARCHAR(128) DEFAULT NULL COMMENT '流程定义名称',
+	`name` VARCHAR(128) DEFAULT NULL COMMENT '流程定义name属性名称',
 	`version` BIGINT(20) DEFAULT NULL COMMENT '版本号，用于获取最新的流程定义',
 	`resource_name` VARCHAR(128) DEFAULT NULL COMMENT '资源文件的名称(通常指流程定义的xml文件)',
 	`resource` text COMMENT '流程定义的资源内容(通常是xml中的内容)',
@@ -26,12 +26,14 @@ DROP TABLE IF EXISTS `workflow_definition_nodes`;
 CREATE TABLE IF NOT EXISTS `workflow_definition_nodes`(
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
 	`flow_definition_id` BIGINT(20) DEFAULT NULL COMMENT '流程定义id',
-	`name` VARCHAR(32) DEFAULT NULL COMMENT '节点名称',
+	`name` VARCHAR(32) DEFAULT NULL COMMENT '节点name属性名称',
+	`node_name` VARCHAR(32) DEFAULT NULL COMMENT '节点标签名称',
 	`type` SMALLINT(6) DEFAULT NULL COMMENT '节点类型',  				-- TODO:描述
 	`assignee` VARCHAR(32) DEFAULT NULL COMMENT '处理人(暂时保留)',
 	`point_x` DOUBLE DEFAULT NULL COMMENT '节点x轴偏移量',
 	`point_y` DOUBLE DEFAULT NULL COMMENT '节点y轴偏移量',
 	`created_at` DATETIME DEFAULT NULL COMMENT '创建时间',
+	`updated_at`  DATETIME DEFAULT NULL COMMENT '更新时间',
 	PRIMARY KEY(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE INDEX idx_flow_definition_nodes_def_id ON workflow_definition_nodes(flow_definition_id);
@@ -43,7 +45,7 @@ CREATE INDEX idx_flow_definition_nodes_def_id ON workflow_definition_nodes(flow_
 DROP TABLE IF EXISTS `workflow_definition_node_events`;
 CREATE TABLE IF NOT EXISTS `workflow_definition_node_events`(
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-	`name` VARCHAR(128) DEFAULT NULL COMMENT '连线名称',
+	`name` VARCHAR(128) DEFAULT NULL COMMENT '连线name属性名称',
 	`flow_definition_id` BIGINT(20) DEFAULT NULL COMMENT '流程定义id,冗余',
 	`source_node_id` BIGINT(20) DEFAULT NULL COMMENT '流程源节点的id',
 	`handler` VARCHAR(128) DEFAULT NULL COMMENT '事件驱动处理类(一般为类标识)',
@@ -51,6 +53,7 @@ CREATE TABLE IF NOT EXISTS `workflow_definition_node_events`(
 	`target_node_id` BIGINT(20) DEFAULT NULL COMMENT '驱动的目标节点id',
 	`describe` text COMMENT '连线描述',
 	`created_at` DATETIME DEFAULT NULL COMMENT '创建时间',
+	`updated_at`  DATETIME DEFAULT NULL COMMENT '更新时间',
 	PRIMARY KEY(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE INDEX idx_flow_node_event_def_id ON workflow_definition_node_events(flow_definition_id);
@@ -94,6 +97,7 @@ CREATE TABLE IF NOT EXISTS `workflow_processes`(
 	`status` SMALLINT(6) DEFAULT NULL COMMENT '流程节点的状态',		-- TODO: 描述
 	`assignee` VARCHAR(32) DEFAULT NULL COMMENT '处理人(暂时保留),该值优先于流程定义节点的值',
 	`created_at` DATETIME DEFAULT NULL COMMENT '创建时间',
+	`updated_at`  DATETIME DEFAULT NULL COMMENT '更新时间',
 	PRIMARY KEY(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE INDEX idx_flow_process_ins_id ON workflow_processes(flow_instance_id);
