bb27f5d782a146e3e80dc0eab14a5b4b29335e4c
DreamYoung <yangzhiliang1018@gmail.com>
2016-05-19 13:55:04 +0800

    add android,ios

--- doctor-open/src/main/java/io/terminus/doctor/open/common/OPSecurityManager.java
+++ doctor-open/src/main/java/io/terminus/doctor/open/common/OPSecurityManager.java
@@ -1,8 +1,11 @@
 package io.terminus.doctor.open.common;
 
+import com.google.common.collect.Maps;
 import io.terminus.pampas.openplatform.core.SecurityManager;
 import io.terminus.pampas.openplatform.entity.OPClientInfo;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Mail: xiao@terminus.io <br>
@@ -11,12 +14,19 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class OPSecurityManager implements SecurityManager {
-    
+
+    private final Map<String, OPClientInfo> clintMap;
+
+    public OPSecurityManager() {
+        clintMap = Maps.newHashMap();
+        clintMap.put("pigDoctorAndroid", new OPClientInfo(1L, "pigDoctorAndroid", "pigDoctorAndroidSecret"));
+        clintMap.put("pigDoctorIOS", new OPClientInfo(2L, "pigDoctorIOS", "pigDoctorIOSSecret"));
+    }
+
     @Override
     public OPClientInfo findClientByAppKey(String appKey) {
         // TODO: 16/5/18 这里要放到数据库里
-
-        return new OPClientInfo(1L, "pigDoctorMobile", "pigDoctorSecret");
+        return clintMap.get(appKey);
     }
 
     @Override
