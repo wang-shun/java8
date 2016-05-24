bcfb26b24dd463076784531b82ea6f138aed4425
yaoqijun <yaoqijunmail@foxmail.com>
2016-05-24 15:34:36 +0800

    fix config

--- doctor-event/src/main/java/io/terminus/doctor/event/DoctorEventConfiguration.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/DoctorEventConfiguration.java
@@ -1,5 +1,20 @@
 package io.terminus.doctor.event;
 
+import io.terminus.boot.mybatis.autoconfigure.MybatisAutoConfiguration;
+import io.terminus.doctor.event.dao.DoctorPigDao;
+import io.terminus.doctor.event.search.pig.BasePigQueryBuilder;
+import io.terminus.doctor.event.search.pig.DefaultIndexedPigFactory;
+import io.terminus.doctor.event.search.pig.DefaultPigQueryBuilder;
+import io.terminus.doctor.event.search.pig.IndexedPig;
+import io.terminus.doctor.event.search.pig.IndexedPigFactory;
+import io.terminus.doctor.event.search.pig.PigSearchProperties;
+import io.terminus.search.core.ESClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +27,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {
         "io.terminus.doctor.event",
-        "io.terminus.doctor.workflow"
+        "io.terminus.doctor.workflow",
 })
+@AutoConfigureAfter(MybatisAutoConfiguration.class)
 public class DoctorEventConfiguration {
 
+    @Configuration
+    @ConditionalOnClass(ESClient.class)
+    @ComponentScan({"io.terminus.search.api"})
+    public static class SearchConfiguration {
+
+        @Bean
+        public ESClient esClient(@Value("${search.host:localhost}") String host,
+                                 @Value("${search.port:9200}") Integer port) {
+            return new ESClient(host, port);
+        }
+
+        @Configuration
+        @EnableConfigurationProperties(PigSearchProperties.class)
+        protected static class PigSearchConfiguration {
+            @Bean
+            @ConditionalOnMissingBean(IndexedPigFactory.class)
+            public IndexedPigFactory<? extends IndexedPig> indexedPigFactory(DoctorPigDao doctorPigDao) {
+                return new DefaultIndexedPigFactory(doctorPigDao);
+            }
+
+            @Bean
+            @ConditionalOnMissingBean(BasePigQueryBuilder.class)
+            public BasePigQueryBuilder pigQueryBuilder() {
+                return new DefaultPigQueryBuilder();
+            }
+
+        }
+
+    }
 }
