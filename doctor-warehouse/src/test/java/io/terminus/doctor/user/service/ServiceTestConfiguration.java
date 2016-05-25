2c4ab370210b684ce1968a43a12e03cab213deca
yaoqijun <yaoqijunmail@foxmail.com>
2016-05-25 10:37:17 +0800

    fix name

---
+++ doctor-warehouse/src/test/java/io/terminus/doctor/user/service/ServiceTestConfiguration.java
@@ -0,0 +1,17 @@
+package io.terminus.doctor.user.service;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-05-25
+ * Email:yaoqj@terminus.io
+ * Descirbe: Service 信息配置工具类
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("io.terminus.doctor.warehouse")
+public class ServiceTestConfiguration {
+}
