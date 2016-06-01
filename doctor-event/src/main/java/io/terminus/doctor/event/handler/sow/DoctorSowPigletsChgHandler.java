65e1ef638e1567cf31e3e013c59f2354c0034ad7
yaoqijun <yaoqijunmail@foxmail.com>
2016-06-01 14:03:07 +0800

    fix

---
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/sow/DoctorSowPigletsChgHandler.java
@@ -0,0 +1,53 @@
+package io.terminus.doctor.event.handler.sow;
+
+import io.terminus.doctor.event.dao.DoctorPigDao;
+import io.terminus.doctor.event.dao.DoctorPigEventDao;
+import io.terminus.doctor.event.dao.DoctorPigSnapshotDao;
+import io.terminus.doctor.event.dao.DoctorPigTrackDao;
+import io.terminus.doctor.event.dao.DoctorRevertLogDao;
+import io.terminus.doctor.event.dto.DoctorBasicInputInfoDto;
+import io.terminus.doctor.event.enums.SowStatus;
+import io.terminus.doctor.event.handler.DoctorAbstractEventFlowHandler;
+import io.terminus.doctor.event.model.DoctorPigTrack;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-06-01
+ * Email:yaoqj@terminus.io
+ * Descirbe: 仔猪变动事件 （仔猪转出信息的录入方式）
+ */
+public class DoctorSowPigletsChgHandler extends DoctorAbstractEventFlowHandler{
+
+    public DoctorSowPigletsChgHandler(DoctorPigDao doctorPigDao, DoctorPigEventDao doctorPigEventDao,
+                                      DoctorPigTrackDao doctorPigTrackDao, DoctorPigSnapshotDao doctorPigSnapshotDao,
+                                      DoctorRevertLogDao doctorRevertLogDao) {
+        super(doctorPigDao, doctorPigEventDao, doctorPigTrackDao, doctorPigSnapshotDao, doctorRevertLogDao);
+    }
+
+    @Override
+    public DoctorPigTrack updateDoctorPigTrackInfo(DoctorPigTrack doctorPigTrack, DoctorBasicInputInfoDto basic, Map<String, Object> extra) {
+        // 校验母猪的状态信息
+        checkState(Objects.equals(doctorPigTrack.getStatus(), SowStatus.FEED.getKey()), "piglets.chgLocation.error");
+
+        // 校验转出的数量信息
+        Map<String,Object> extraMap = doctorPigTrack.getExtraMap();
+        Integer healthPigCount = (Integer) extraMap.get("healthCount");
+        Integer partWeanPigletsCount = 0;
+        if(extraMap.containsKey("partWeanPigletsCount")){
+            partWeanPigletsCount = (Integer) extraMap.get("partWeanPigletsCount");
+        }
+        Integer pigletsCount = (Integer) extra.get("pigletsCount"); //对应的仔猪变动的数量
+        checkState((healthPigCount - partWeanPigletsCount)>= pigletsCount, "piglets.chgCount.error");
+
+        healthPigCount = healthPigCount - pigletsCount; // 当前健康的数量
+        extra.put("healthCount", healthPigCount);
+        doctorPigTrack.addAllExtraMap(extra);
+
+        return doctorPigTrack;
+    }
+}
