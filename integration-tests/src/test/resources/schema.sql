3d5e17757dca0d9022a198bdb7296643888bf550
yaoqijun <yaoqijunmail@foxmail.com>
2016-06-06 15:05:49 +0800

    modify schemal

--- integration-tests/src/test/resources/schema.sql
+++ integration-tests/src/test/resources/schema.sql
@@ -491,6 +491,7 @@ CREATE TABLE `doctor_group_events` (
   PRIMARY KEY (`id`)
 )  COMMENT='猪群事件表';
 
+-- 猪
 CREATE TABLE `doctor_pigs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `org_id` bigint(20) unsigned DEFAULT NULL COMMENT '公司Id',
@@ -500,8 +501,8 @@ CREATE TABLE `doctor_pigs` (
   `out_id` varchar(128) DEFAULT NULL COMMENT '关联猪外部Id',
   `pig_code` varchar(64) DEFAULT NULL COMMENT '猪编号',
   `pig_type` smallint(6) DEFAULT NULL COMMENT '猪类型(公猪，母猪， 仔猪)',
-  `pig_father_id` bigint(20) unsigned DEFAULT NULL COMMENT '猪父亲Id',
-  `pig_mother_id` bigint(20) unsigned DEFAULT NULL COMMENT '母猪Id',
+  `pig_father_code` VARCHAR(64) unsigned DEFAULT NULL COMMENT '猪父亲Id',
+  `pig_mother_code` VARCHAR(64) unsigned DEFAULT NULL COMMENT '母猪Id',
   `source` smallint(6) DEFAULT NULL COMMENT '母猪来源',
   `birth_date` datetime DEFAULT NULL COMMENT '母猪生日',
   `birth_weight` double DEFAULT NULL COMMENT '出生重量',
@@ -522,8 +523,9 @@ CREATE TABLE `doctor_pigs` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-)  COMMENT='猪基础信息表';
+) COMMENT='猪基础信息表';
 
+-- 猪 track 信息关联表
 CREATE TABLE `doctor_pig_tracks` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `farm_id` bigint(20) unsigned NOT NULL comment '猪场Id',
@@ -533,7 +535,7 @@ CREATE TABLE `doctor_pig_tracks` (
   `current_barn_name` varchar(64) DEFAULT NULL COMMENT '当前猪舍名称',
   `weight` double DEFAULT NULL COMMENT '猪重量',
   `out_farm_date` datetime DEFAULT NULL COMMENT '猪离场时间',
-  `rel_event_id` bigint(20) DEFAULT NULL COMMENT '关联事件最近事件',
+  `rel_event_ids` text DEFAULT NULL COMMENT '关联事件最近事件',
   `extra` text COMMENT '事件修改猪对应信息',
   `current_parity` int(11) DEFAULT NULL COMMENT '当前胎次信息',
   `remark` text COMMENT '备注',
@@ -544,25 +546,22 @@ CREATE TABLE `doctor_pig_tracks` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-)  COMMENT='猪Track 信息表';
+) COMMENT='猪Track 信息表';
 
-
-
-drop table if exists doctor_pig_snapshots;
+-- 添加猪快照信息表数据内容
 create table doctor_pig_snapshots(
-  `id` bigint(20) unsigned not null AUTO_INCREMENT comment 'id',
-  `org_id` bigint(20) unsigned default null comment 'org_id',
-  `farm_id` bigint(20) unsigned default null comment 'farm_id',
-  `pig_id` bigint(20) unsigned default null comment 'pig_id',
-  `event_id` bigint(20) unsigned default null comment 'event_id',
-  `pig_info` text default null comment '公猪快照信息',
-  `created_at` datetime DEFAULT NULL,
-    `updated_at` datetime DEFAULT NULL,
-     primary key(id)
+	`id` bigint(20) unsigned not null AUTO_INCREMENT comment 'id',
+	`org_id` bigint(20) unsigned default null comment 'org_id',
+	`farm_id` bigint(20) unsigned default null comment 'farm_id',
+	`pig_id` bigint(20) unsigned default null comment 'pig_id',
+	`event_id` bigint(20) unsigned default null comment 'event_id',
+	`pig_info` text default null comment '公猪快照信息',
+	`created_at` datetime DEFAULT NULL,
+  	`updated_at` datetime DEFAULT NULL,
+  	 primary key(id)
 ) COMMENT='猪群事件记录数据表';
 
-
-
+-- 公猪，母猪， 仔猪事件信息表
 CREATE TABLE `doctor_pig_events` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
   `org_id` bigint(20) unsigned DEFAULT NULL COMMENT '公司Id',
@@ -589,9 +588,7 @@ CREATE TABLE `doctor_pig_events` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-)  COMMENT='用户设备信息表';
-
-
+) COMMENT='用户设备信息表';
 
 drop Table if exists doctor_vaccination_pig_warns;
 create table doctor_vaccination_pig_warns (
