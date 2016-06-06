422e5052a3df2edc9c8b0e93a483fedc4415c906
陈增辉 <zenghui@terminus.io>
2016-06-06 12:00:00 +0800

    add series code of DoctorServiceStatus

--- doctor-user-api/src/main/java/io/terminus/doctor/user/dto/DoctorServiceStatusDto.java
+++ doctor-user-api/src/main/java/io/terminus/doctor/user/dto/DoctorServiceStatusDto.java
@@ -1,6 +1,5 @@
 package io.terminus.doctor.user.dto;
 
-import io.terminus.doctor.user.model.DoctorServiceReview;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -18,15 +17,14 @@ public class DoctorServiceStatusDto implements Serializable {
     private Long userId;
 
     /**
-     * 服务状态, 0:未提交申请;  2:已提交申请,正在审核中;  1:已开通;  -1:审核不通过;  -2:冻结申请资格;
-     * 其他3各状态值与此相同
+     * 以下4个是服务的状态, 0:未提交申请;  2:已提交申请,正在审核中;  1:已开通;  -1:审核不通过;  -2:冻结申请资格;
      */
     private Integer pigDoctor;  //猪场软件
     private Integer pigmall;    //新融电商
     private Integer neverest;   //大数据
     private Integer pigTrade;   //生猪交易
 
-    //下面4个是服务被冻结或审核不通过的原因
+    //下面4个是审核不通过或申请资格被冻结的原因
     private String pigDoctorReason;
     private String pigmallReason;
     private String neverestReason;
