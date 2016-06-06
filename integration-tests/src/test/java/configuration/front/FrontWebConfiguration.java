4af3d14a72dee5f9f931a4694a131d4212116aa8
houly <houly@terminus.io>
2016-06-06 14:35:02 +0800

    fix test

--- integration-tests/src/test/java/configuration/front/FrontWebConfiguration.java
+++ integration-tests/src/test/java/configuration/front/FrontWebConfiguration.java
@@ -13,6 +13,7 @@ import io.terminus.doctor.web.core.msg.email.CommonEmailServiceConfig;
 import io.terminus.doctor.web.core.msg.sms.LuoSiMaoSmsServiceConfig;
 import io.terminus.doctor.web.core.service.OtherSystemServiceConfig;
 import io.terminus.pampas.openplatform.core.Gateway;
+import io.terminus.parana.auth.core.AuthenticationConfiguration;
 import io.terminus.parana.web.msg.config.MsgWebConfig;
 import io.terminus.parana.web.msg.config.db.DbAppPushConfig;
 import io.terminus.parana.web.msg.config.db.DbEmailConfig;
@@ -46,6 +47,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
         DoctorWarehouseConfiguration.class,
         DoctorCoreWebConfiguration.class,
         OtherSystemServiceConfig.class,
+        AuthenticationConfiguration.class,
         MsgWebConfig.class,
         LuoSiMaoSmsServiceConfig.class,
         CommonEmailServiceConfig.class
