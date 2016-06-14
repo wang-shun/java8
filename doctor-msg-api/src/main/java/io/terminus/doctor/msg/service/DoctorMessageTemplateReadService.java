e2cef1c3dfec68c85863cde7df826a8b2d77895e
DreamYoung <yangzhiliang1018@gmail.com>
2016-06-14 21:30:39 +0800

    add template cache

--- doctor-msg-api/src/main/java/io/terminus/doctor/msg/service/DoctorMessageTemplateReadService.java
+++ doctor-msg-api/src/main/java/io/terminus/doctor/msg/service/DoctorMessageTemplateReadService.java
@@ -3,6 +3,7 @@ package io.terminus.doctor.msg.service;
 import io.terminus.common.model.Response;
 import io.terminus.parana.msg.dto.MessageInfo;
 import io.terminus.parana.msg.service.MessageTemplateReadService;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -41,4 +42,18 @@ public interface DoctorMessageTemplateReadService extends MessageTemplateReadSer
      * @return 应用消息模板后生成的标题和消息内容
      */
     Response<MessageInfo> getMessageInfoWithCache(String templateName, Map<String, Serializable> context);
+
+    /**
+     * 清理所有message模板缓存
+     *
+     * @return 是否成功
+     */
+    Response<Boolean> cleanAllMessageTemplateCache();
+
+    /**
+     * 根据模板名称重新加载message模板缓存
+     *
+     * @return 是否成功
+     */
+    Response<Boolean> reloadAllMessageTemplateByName(@NotEmpty(message = "template.name.not.empty") String templateName);
 }
--- doctor-msg/src/main/java/io/terminus/doctor/msg/service/DoctorMessageTemplateReadServiceImpl.java
+++ doctor-msg/src/main/java/io/terminus/doctor/msg/service/DoctorMessageTemplateReadServiceImpl.java
@@ -1,7 +1,10 @@
 package io.terminus.doctor.msg.service;
 
+import com.github.jknack.handlebars.Template;
 import com.google.common.base.Throwables;
+import io.terminus.common.exception.ServiceException;
 import io.terminus.common.model.Response;
+import io.terminus.doctor.msg.helper.DoctorMessageTemplateCacher;
 import io.terminus.parana.msg.dto.MessageInfo;
 import io.terminus.parana.msg.impl.dao.mysql.MessageTemplateDao;
 import io.terminus.parana.msg.impl.service.MessageTemplateReadServiceImpl;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Desc:
@@ -24,11 +29,14 @@ import java.util.Map;
 public class DoctorMessageTemplateReadServiceImpl extends MessageTemplateReadServiceImpl implements DoctorMessageTemplateReadService {
 
     private final MessageTemplateDao messageTemplateDao;
+    private final DoctorMessageTemplateCacher doctorMessageTemplateCacher;
 
     @Autowired
-    public DoctorMessageTemplateReadServiceImpl(MessageTemplateDao messageTemplateDao) {
+    public DoctorMessageTemplateReadServiceImpl(MessageTemplateDao messageTemplateDao,
+                                                DoctorMessageTemplateCacher doctorMessageTemplateCacher) {
         super(messageTemplateDao);
         this.messageTemplateDao = messageTemplateDao;
+        this.doctorMessageTemplateCacher = doctorMessageTemplateCacher;
     }
 
     @Override
@@ -69,16 +77,79 @@ public class DoctorMessageTemplateReadServiceImpl extends MessageTemplateReadSer
 
     @Override
     public Response<String> getMessageTitleWithCache(String templateName, Map<String, Serializable> context) {
-        return getMessageTitle(templateName, context);
+        try {
+            return Response.ok(getTitleTemlate(templateName).apply(context));
+        } catch (ServiceException e) {
+            return Response.fail(e.getMessage());
+        } catch (Exception e) {
+            log.error("get message title with cache failed, templateName:{}, context:{}, cause:{}", templateName, context, Throwables.getStackTraceAsString(e));
+            return Response.fail("get.message.title.failed");
+        }
     }
 
     @Override
     public Response<String> getMessageContentWithCache(String templateName, Map<String, Serializable> context) {
-        return getMessageContent(templateName, context);
+        try {
+            return Response.ok(getContentTemlate(templateName).apply(context));
+        } catch (ServiceException e) {
+            return Response.fail(e.getMessage());
+        } catch (Exception e) {
+            log.error("get message content with cache failed, templateName:{}, context:{}, cause:{}", templateName, context, Throwables.getStackTraceAsString(e));
+            return Response.fail("get.message.content.failed");
+        }
     }
 
     @Override
     public Response<MessageInfo> getMessageInfoWithCache(String templateName, Map<String, Serializable> context) {
-        return getMessageInfo(templateName, context);
+        try {
+            MessageInfo messageInfo = new MessageInfo();
+            messageInfo.setMessageTitle(getTitleTemlate(templateName).apply(context));
+            messageInfo.setMessageContent(getContentTemlate(templateName).apply(context));
+            return Response.ok(messageInfo);
+        } catch (ServiceException e) {
+            return Response.fail(e.getMessage());
+        } catch (Exception e) {
+            log.error("get message info with cache failed, templateName:{}, context:{}, cause:{}", templateName, context, Throwables.getStackTraceAsString(e));
+            return Response.fail("get.message.info.failed");
+        }
+    }
+
+    @Override
+    public Response<Boolean> cleanAllMessageTemplateCache() {
+        try {
+            doctorMessageTemplateCacher.cleanAll();
+            return Response.ok(Boolean.TRUE);
+        } catch (Exception e) {
+            log.error("clean all message template cache failed, cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("message.template.cache.clean.fail");
+        }
+    }
+
+    @Override
+    public Response<Boolean> reloadAllMessageTemplateByName(String templateName) {
+        try {
+            doctorMessageTemplateCacher.refreshByName(templateName);
+            return Response.ok(Boolean.TRUE);
+        } catch (Exception e) {
+            log.error("reload all message template cache by name failed, templateName:{}, cause:{}",
+                    templateName, Throwables.getStackTraceAsString(e));
+            return Response.fail("message.template.cache.reload.fail");
+        }
+    }
+
+    private Template getTitleTemlate(String name) throws ExecutionException {
+        Optional<Template> template = doctorMessageTemplateCacher.getTemplateTitleCache().get(name);
+        if (template.isPresent()) {
+            return template.get();
+        }
+        throw new ServiceException("message.title.template.not.found");
+    }
+
+    private Template getContentTemlate(String name) throws ExecutionException {
+        Optional<Template> template = doctorMessageTemplateCacher.getTemplateContentCache().get(name);
+        if (template.isPresent()) {
+            return template.get();
+        }
+        throw new ServiceException("message.content.template.not.found");
     }
 }
