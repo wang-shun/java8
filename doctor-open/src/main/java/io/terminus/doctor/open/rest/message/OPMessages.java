accb371c26c6ab59edb14d53a6feaea47da68841
DreamYoung <yangzhiliang1018@gmail.com>
2016-06-16 17:34:05 +0800

    rm mock

--- doctor-open/src/main/java/io/terminus/doctor/open/rest/message/OPMessages.java
+++ doctor-open/src/main/java/io/terminus/doctor/open/rest/message/OPMessages.java
@@ -1,8 +1,11 @@
 package io.terminus.doctor.open.rest.message;
 
-import io.terminus.doctor.common.utils.RandomUtil;
+import io.terminus.doctor.msg.service.DoctorMessageReadService;
+import io.terminus.doctor.open.util.OPRespHelper;
+import io.terminus.pampas.common.UserUtil;
 import io.terminus.pampas.openplatform.annotations.OpenBean;
 import io.terminus.pampas.openplatform.annotations.OpenMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Desc:
@@ -14,12 +17,19 @@ import io.terminus.pampas.openplatform.annotations.OpenMethod;
 @SuppressWarnings("unused")
 public class OPMessages {
 
+    private final DoctorMessageReadService doctorMessageReadService;
+
+    @Autowired
+    public OPMessages(DoctorMessageReadService doctorMessageReadService) {
+        this.doctorMessageReadService = doctorMessageReadService;
+    }
+
     /**
      * 获取未读消息数量
      * @return 未读消息数量
      */
     @OpenMethod(key = "get.unread.message")
     public Integer getUnReadMessages() {
-        return RandomUtil.random(0, 10);
+        return OPRespHelper.orOPEx(doctorMessageReadService.findNoReadCount(UserUtil.getUserId())).intValue();
     }
 }
