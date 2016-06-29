96153bd800da2ec6b0394e87fe2f70370d57e6de
IceMimosa <chk@terminus.io>
2016-06-29 19:19:47 +0800

    add search barn dto

---
+++ doctor-event-api/src/main/java/io/terminus/doctor/event/search/barn/SearchedBarnDto.java
@@ -0,0 +1,15 @@
+package io.terminus.doctor.event.search.barn;
+
+import java.io.Serializable;
+
+/**
+ * Desc: 搜索猪舍相关封装
+ * Mail: chk@terminus.io
+ * Created by IceMimosa
+ * Date: 16/6/29
+ */
+public class SearchedBarnDto implements Serializable {
+    private static final long serialVersionUID = 8736309253435207762L;
+
+
+}
