711e56f41bc95a9818e815762c1f658f29dfb5ec
DreamYoung <yangzhiliang1018@gmail.com>
2016-07-20 23:22:33 +0800

    just for mock

--- doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorDailyReports.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorDailyReports.java
@@ -3,6 +3,7 @@ package io.terminus.doctor.web.front.event.controller;
 import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import io.terminus.doctor.common.utils.RespHelper;
 import io.terminus.doctor.event.dto.report.DoctorDailyReportDto;
+import io.terminus.doctor.event.dto.report.DoctorLiveStockDailyReport;
 import io.terminus.doctor.event.service.DoctorDailyReportReadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,18 @@ public class DoctorDailyReports {
     @RequestMapping(value = "/daily", method = RequestMethod.GET)
     public DoctorDailyReportDto findDailyReportByFarmIdAndSumAtWithCache(@RequestParam("farmId") Long farmId,
                                                                          @RequestParam("date") String date) {
-        return RespHelper.or500(doctorDailyReportReadService.findDailyReportByFarmIdAndSumAtWithCache(farmId, date));
+        DoctorLiveStockDailyReport report = new DoctorLiveStockDailyReport();
+        report.setFarrow(15);
+        report.setFatten(14);
+        report.setBoar(5);
+        report.setPeihuaiSow(1);
+        report.setNursery(90);
+        report.setHoubeiSow(10);
+        report.setKonghuaiSow(5);
+        DoctorDailyReportDto d = new DoctorDailyReportDto();
+        d.setLiveStock(report);
+        return d;
+//        return RespHelper.or500(doctorDailyReportReadService.findDailyReportByFarmIdAndSumAtWithCache(farmId, date));
     }
 
     /**
