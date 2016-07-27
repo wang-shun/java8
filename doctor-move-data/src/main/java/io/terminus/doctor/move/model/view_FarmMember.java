f4c627b6e3ae7dec20732b8297c68e44728ade35
陈增辉 <zenghui@terminus.io>
2016-07-27 17:27:19 +0800

    add move data model

---
+++ doctor-move-data/src/main/java/io/terminus/doctor/move/model/view_FarmMember.java
@@ -0,0 +1,32 @@
+package io.terminus.doctor.move.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by chenzenghui on 16/7/27.
+ */
+public class view_FarmMember implements Serializable{
+    private String OID;
+    private String Sex;
+    private String MobilPhone;
+    private String OfficePhoneNum;
+    private String Duty;
+    private String URL;
+    private String OrgOID;
+    private String OrganizeName;
+    private String UpOID;
+    private String FarmOID;
+    private String IsStopUse;
+    private String Status;
+    private Integer Ord;
+    private String LoginInfoOID;
+    private String LoginName;
+    private String LoginPassword;
+    private String Srm;
+    private String rtOID;
+    private String RoleName;
+    private String RoleOID;
+    private String mrOID;
+    private Integer Levels;
+    private String FarmName;
+}
