9371ba3d890d683aa18b69ce30ab202594c16d81
陈增辉 <zenghui@terminus.io>
2016-07-27 16:54:33 +0800

    add move data model

--- doctor-move-data/src/main/java/io/terminus/doctor/move/handler/DoctorMoveTableEnum.java
+++ doctor-move-data/src/main/java/io/terminus/doctor/move/handler/DoctorMoveTableEnum.java
@@ -1,6 +1,17 @@
 package io.terminus.doctor.move.handler;
 
+import io.terminus.doctor.move.model.B_ChangeReason;
+import io.terminus.doctor.move.model.B_Customer;
 import io.terminus.doctor.move.model.TB_FieldValue;
+import io.terminus.doctor.move.model.View_BoarCardList;
+import io.terminus.doctor.move.model.View_EventListBoar;
+import io.terminus.doctor.move.model.View_EventListGain;
+import io.terminus.doctor.move.model.View_EventListSow;
+import io.terminus.doctor.move.model.View_FarmInfo;
+import io.terminus.doctor.move.model.View_FarmMember;
+import io.terminus.doctor.move.model.View_GainCardList;
+import io.terminus.doctor.move.model.View_PigLocationList;
+import io.terminus.doctor.move.model.View_SowCardList;
 import lombok.Getter;
 
 /**
@@ -13,34 +24,34 @@ import lombok.Getter;
 public enum DoctorMoveTableEnum {
 
     //基础数据
-    TB_FieldValue(TB_FieldValue.class);
+    TB_FieldValue(TB_FieldValue.class),
 
-//    //变动原因
-//    B_ChangeReason,
-//
-//    //客户
-//    B_Customer,
-//
-//    //公司猪场
-//    view_FarmInfo,
-//
-//    //职员
-//    view_FarmMember,
-//
-//    //猪舍
-//    view_PigLocationList,
-//
-//    //猪群
-//    view_GainCardList,
-//    view_EventListGain,
-//
-//    //公猪
-//    view_BoarCardList,
-//    view_EventListBoar,
-//
-//    //母猪
-//    view_SowCardList,
-//    view_EventListSow;
+    //变动原因
+    B_ChangeReason(B_ChangeReason.class),
+
+    //客户
+    B_Customer(B_Customer.class),
+
+    //公司猪场
+    view_FarmInfo(View_FarmInfo.class),
+
+    //职员
+    view_FarmMember(View_FarmMember.class),
+
+    //猪舍
+    view_PigLocationList(View_PigLocationList.class),
+
+    //猪群
+    view_GainCardList(View_GainCardList.class),
+    view_EventListGain(View_EventListGain.class),
+
+    //公猪
+    view_BoarCardList(View_BoarCardList.class),
+    view_EventListBoar(View_EventListBoar.class),
+
+    //母猪
+    view_SowCardList(View_SowCardList.class),
+    view_EventListSow(View_EventListSow.class);
 
     @Getter
     private final Class clazz;
