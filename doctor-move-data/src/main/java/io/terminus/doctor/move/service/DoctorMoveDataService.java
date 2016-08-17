2b94a21daad6182ed7dbfee1e9a07cea67ef9424
DreamYoung <yangzhiliang1018@gmail.com>
2016-08-18 01:13:45 +0800

    fix map json

--- doctor-move-data/src/main/java/io/terminus/doctor/move/service/DoctorMoveDataService.java
+++ doctor-move-data/src/main/java/io/terminus/doctor/move/service/DoctorMoveDataService.java
@@ -168,7 +168,8 @@ public class DoctorMoveDataService {
      */
     public void updateFarrowSow(DoctorFarm farm) {
         //猪场号, 猪群id
-        Map<String, Long> groupMap = doctorGroupDao.findByFarmId(farm.getId()).stream().collect(Collectors.toMap(DoctorGroup::getOutId, DoctorGroup::getId));
+        Map<String, Long> groupMap = Maps.newHashMap();
+        doctorGroupDao.findByFarmId(farm.getId()).forEach(group -> groupMap.put(group.getGroupCode(), group.getId()));
 
         //只更新未离场的吧
         doctorPigTrackDao.list(DoctorPigTrack.builder()
@@ -179,6 +180,7 @@ public class DoctorMoveDataService {
                     Map<String, Object> extraMap = JSON_MAPPER.fromJson(track.getExtra(), JSON_MAPPER.createCollectionType(Map.class, String.class, Object.class));
                     if (extraMap.containsKey("groupCode")) {
                         extraMap.put("farrowingPigletGroupId", groupMap.get(String.valueOf(extraMap.get("groupCode"))));
+                        track.setExtra(JSON_MAPPER.toJson(extraMap));
                         doctorPigTrackDao.update(track);
                     }
                 });
