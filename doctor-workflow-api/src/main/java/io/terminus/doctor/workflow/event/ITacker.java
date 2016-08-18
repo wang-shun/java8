1e53f5588d8a40b272a0c8f387bb0e71f75a7722
xiaojiannan <xiaojiannan@terminus.io>
2016-08-18 10:48:14 +0800

    fixd

--- doctor-workflow-api/src/main/java/io/terminus/doctor/workflow/event/ITacker.java
+++ doctor-workflow-api/src/main/java/io/terminus/doctor/workflow/event/ITacker.java
@@ -4,5 +4,10 @@ package io.terminus.doctor.workflow.event;
  * Created by xiao on 16/8/17.
  */
 public interface ITacker {
+    /**
+     * 母猪配种次数处理
+     * @param flowData
+     * @return
+     */
     Boolean tacker(String flowData);
 }
