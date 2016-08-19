3e083b368a81d43f4114455a296afc4ddccd5b51
IceMimosa <chk@terminus.io>
2016-08-19 11:46:24 +0800

    fix

--- doctor-event/src/main/java/io/terminus/doctor/event/handler/sow/DoctorSowChgLocationCheckTacker.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/sow/DoctorSowChgLocationCheckTacker.java
@@ -1,7 +1,9 @@
 package io.terminus.doctor.event.handler.sow;
 
 import io.terminus.doctor.common.enums.PigType;
+import io.terminus.doctor.event.dao.DoctorBarnDao;
 import io.terminus.doctor.event.dao.DoctorPigTrackDao;
+import io.terminus.doctor.event.model.DoctorBarn;
 import io.terminus.doctor.event.model.DoctorPigTrack;
 import io.terminus.doctor.workflow.core.TackerExecution;
 import io.terminus.doctor.workflow.event.ITacker;
@@ -15,16 +17,23 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class DoctorSowChgLocationCheckTacker implements ITacker {
+
     @Autowired
     private DoctorPigTrackDao doctorPigTrackDao;
+
+    @Autowired
+    private DoctorBarnDao doctorBarnDao;
+
     @Override
     public Boolean tacker(TackerExecution tackerExecution) {
         if (tackerExecution != null){
             DoctorPigTrack doctorPigTrack = doctorPigTrackDao.findByPigId(tackerExecution.getBusinessId());
             if (doctorPigTrack != null){
-                Integer currentBarnTyp = doctorPigTrack.getCurrentBarnType();
-                if (currentBarnTyp != null){
-                    if (currentBarnTyp == PigType.PREG_SOW.getValue()){
+                Long barnId = doctorPigTrack.getCurrentBarnId();
+                DoctorBarn barn = doctorBarnDao.findById(barnId);
+                if (barn != null && barn.getPigType() != null){
+                    // 猪类型是妊娠舍
+                    if (barn.getPigType() == PigType.PREG_SOW.getValue()){
                         return false;
                     }
                 }
