2e8a40ca630bb84e73e3c8e43d6f58d8fff4d367
DreamYoung <yangzhiliang1018@gmail.com>
2016-08-22 16:01:34 +0800

    check group equal

--- doctor-event/src/main/java/io/terminus/doctor/event/handler/group/DoctorTransGroupEventHandler.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/group/DoctorTransGroupEventHandler.java
@@ -69,7 +69,7 @@ public class DoctorTransGroupEventHandler extends DoctorAbstractGroupEventHandle
 
         //校验能否转群, 数量, 日龄差, 转群总重
         checkCanTransBarn(group.getPigType(), transGroup.getToBarnId());
-        checkCanTransGroup(transGroup.getToGroupId(), transGroup.getToBarnId());
+        checkCanTransGroup(group.getId(), transGroup.getToGroupId(), transGroup.getToBarnId());
         checkFarrowGroupUnique(transGroup.getIsCreateGroup(), transGroup.getToBarnId());
         checkQuantity(groupTrack.getQuantity(), transGroup.getQuantity());
         checkQuantityEqual(transGroup.getQuantity(), transGroup.getBoarQty(), transGroup.getSowQty());
