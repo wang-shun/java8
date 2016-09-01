e85803de55132773f6fdc311ec094e68dddd16fb
DreamYoung <yangzhiliang1018@gmail.com>
2016-09-01 18:19:46 +0800

    refine WTF code

--- doctor-event/src/main/java/io/terminus/doctor/event/report/count/DoctorDailyEntryEventCount.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/report/count/DoctorDailyEntryEventCount.java
@@ -1,6 +1,5 @@
 package io.terminus.doctor.event.report.count;
 
-import io.terminus.doctor.common.utils.DateUtil;
 import io.terminus.doctor.event.daily.DoctorDailyEventCount;
 import io.terminus.doctor.event.dao.DoctorKpiDao;
 import io.terminus.doctor.event.dto.report.daily.DoctorDailyReportDto;
@@ -11,13 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
-import static com.google.common.base.Preconditions.checkState;
 
 /**
  * Created by yaoqijun.
@@ -37,32 +30,17 @@ public class DoctorDailyEntryEventCount implements DoctorDailyEventCount {
     }
 
     @Override
-    public List<DoctorPigEvent> preDailyEventHandleValidate(List<DoctorPigEvent> t) {
-        return t.stream().filter(e -> Objects.equals(e.getType(), PigEvent.ENTRY.getKey()) || Objects.equals(e.getType(), PigEvent.REMOVAL.getKey())).collect(Collectors.toList());
+    public boolean preDailyEventHandleValidate(DoctorPigEvent event) {
+        return Objects.equals(event.getType(), PigEvent.ENTRY.getKey()) || Objects.equals(event.getType(), PigEvent.REMOVAL.getKey());
     }
 
     @Override
-    public void dailyEventHandle(List<DoctorPigEvent> t, DoctorDailyReportDto doctorDailyReportDto, Map<String, Object> context) {
-
-        // validate same farm
-        Long farmId;
-        Date eventAt;
-        if(t.size() == 0){
-            farmId = Long.valueOf(context.get("farmId").toString());
-            eventAt = DateUtil.toDate(context.get("eventAt").toString());
-        }else {
-            checkState(t.size() > 0, "dailyEntry.eventCount.fail");
-            Long currentFarmId = t.get(0).getFarmId();
-            t.forEach(e-> checkState(Objects.equals(e.getFarmId(), currentFarmId), "dailyEntry.notSameFarm.fail"));
-            farmId = currentFarmId;
-            eventAt = t.get(0).getEventAt();
-        }
-
+    public void dailyEventHandle(DoctorPigEvent event, DoctorDailyReportDto dailyReportDto) {
         //存栏
-        DoctorLiveStockDailyReport liveStock = doctorDailyReportDto.getLiveStock();
-        liveStock.setBuruSow(doctorKpiDao.realTimeLiveStockFarrowSow(farmId, eventAt));    //产房母猪
-        liveStock.setPeihuaiSow(doctorKpiDao.realTimeLiveStockSow(farmId, eventAt) - liveStock.getBuruSow());    //配怀 = 总存栏 - 产房母猪
+        DoctorLiveStockDailyReport liveStock = dailyReportDto.getLiveStock();
+        liveStock.setBuruSow(doctorKpiDao.realTimeLiveStockFarrowSow(event.getFarmId(), event.getEventAt()));    //产房母猪
+        liveStock.setPeihuaiSow(doctorKpiDao.realTimeLiveStockSow(event.getFarmId(), event.getEventAt()) - liveStock.getBuruSow());    //配怀 = 总存栏 - 产房母猪
         liveStock.setKonghuaiSow(0);                                                       //空怀猪作废, 置成0
-        liveStock.setBoar(doctorKpiDao.realTimeLiveStockBoar(farmId, eventAt));            //公猪
+        liveStock.setBoar(doctorKpiDao.realTimeLiveStockBoar(event.getFarmId(), event.getEventAt()));            //公猪
     }
 }
--- doctor-event/src/main/java/io/terminus/doctor/event/report/count/DoctorDailyFarrowingEventCount.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/report/count/DoctorDailyFarrowingEventCount.java
@@ -1,6 +1,5 @@
 package io.terminus.doctor.event.report.count;
 
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import io.terminus.doctor.event.daily.DoctorDailyEventCount;
 import io.terminus.doctor.event.dto.report.daily.DoctorDailyReportDto;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Created by yaoqijun.
@@ -26,27 +24,20 @@ public class DoctorDailyFarrowingEventCount implements DoctorDailyEventCount{
     private static final List<String> SHMJ = Lists.newArrayList("mnyCount","jxCount","deadCount","blackCount");
 
     @Override
-    public List<DoctorPigEvent> preDailyEventHandleValidate(List<DoctorPigEvent> t) {
-        return t.stream().filter(e-> Objects.equals(e.getType(), PigEvent.FARROWING.getKey())).collect(Collectors.toList());
+    public boolean preDailyEventHandleValidate(DoctorPigEvent event) {
+        return Objects.equals(event.getType(), PigEvent.FARROWING.getKey());
     }
 
     @Override
-    public void dailyEventHandle(List<DoctorPigEvent> t, DoctorDailyReportDto doctorDailyReportDto, Map<String, Object> context) {
-
-        if(Iterables.isEmpty(t))
-            return;
-
+    public void dailyEventHandle(DoctorPigEvent event, DoctorDailyReportDto doctorDailyReportDto) {
         DoctorDeliverDailyReport doctorDeliverDailyReport = new DoctorDeliverDailyReport();
 
-        t.forEach(e -> {
-            Map<String, Object> extraMap = e.getExtraMap();
-
-            doctorDeliverDailyReport.setNest(doctorDeliverDailyReport.getNest() + 1);
-            doctorDeliverDailyReport.setLive(doctorDeliverDailyReport.getLive() + Integer.valueOf(extraMap.get("farrowingLiveCount").toString()));
-            doctorDeliverDailyReport.setHealth(doctorDeliverDailyReport.getHealth() + Integer.valueOf(extraMap.get("healthCount").toString()));
-            doctorDeliverDailyReport.setWeak(doctorDeliverDailyReport.getWeak() + Integer.valueOf(extraMap.get("weakCount").toString()));
-            SHMJ.forEach(s -> doctorDeliverDailyReport.setBlack(doctorDeliverDailyReport.getBlack() + Integer.valueOf(extraMap.get(s).toString())));
-        });
+        Map<String, Object> extraMap = event.getExtraMap();
+        doctorDeliverDailyReport.setNest(doctorDeliverDailyReport.getNest() + 1);
+        doctorDeliverDailyReport.setLive(doctorDeliverDailyReport.getLive() + Integer.valueOf(extraMap.get("farrowingLiveCount").toString()));
+        doctorDeliverDailyReport.setHealth(doctorDeliverDailyReport.getHealth() + Integer.valueOf(extraMap.get("healthCount").toString()));
+        doctorDeliverDailyReport.setWeak(doctorDeliverDailyReport.getWeak() + Integer.valueOf(extraMap.get("weakCount").toString()));
+        SHMJ.forEach(s -> doctorDeliverDailyReport.setBlack(doctorDeliverDailyReport.getBlack() + Integer.valueOf(extraMap.get(s).toString())));
 
         doctorDailyReportDto.getDeliver().addDeliverCount(doctorDeliverDailyReport);
     }
