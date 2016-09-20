4e3651b76a7116ddcf2c79febf6b1a09174bc6a3
DreamYoung <yangzhiliang1018@gmail.com>
2016-09-20 19:41:03 +0800

    add occupy handler

---
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/rollback/group/DoctorRollbackTransGroupHandler.java
@@ -0,0 +1,33 @@
+package io.terminus.doctor.event.handler.rollback.group;
+
+import io.terminus.doctor.event.handler.rollback.DoctorAbstractRollbackGroupEventHandler;
+import io.terminus.doctor.event.model.DoctorGroupEvent;
+import io.terminus.doctor.event.model.DoctorRevertLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Desc: 猪群转群转群回滚
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 16/9/20
+ */
+@Slf4j
+@Component
+public class DoctorRollbackTransGroupHandler extends DoctorAbstractRollbackGroupEventHandler {
+
+    @Override
+    protected boolean handleCheck(DoctorGroupEvent groupEvent) {
+        return false;
+    }
+
+    @Override
+    protected DoctorRevertLog handleRollback(DoctorGroupEvent groupEvent) {
+        return null;
+    }
+
+    @Override
+    public void updateReport(DoctorGroupEvent groupEvent) {
+
+    }
+}
---
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/rollback/sow/DoctorRollbackFarrowHandler.java
@@ -0,0 +1,34 @@
+package io.terminus.doctor.event.handler.rollback.sow;
+
+import io.terminus.doctor.event.handler.rollback.DoctorAbstractRollbackPigEventHandler;
+import io.terminus.doctor.event.model.DoctorPigEvent;
+import io.terminus.doctor.event.model.DoctorRevertLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Desc: 母猪分娩回滚
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 16/9/20
+ */
+@Slf4j
+@Component
+public class DoctorRollbackFarrowHandler extends DoctorAbstractRollbackPigEventHandler {
+
+    @Override
+    protected boolean handleCheck(DoctorPigEvent pigEvent) {
+        return false;
+    }
+
+    @Override
+    protected DoctorRevertLog handleRollback(DoctorPigEvent pigEvent) {
+        return null;
+    }
+
+    @Override
+    public void updateReport(DoctorPigEvent pigEvent) {
+
+    }
+
+}
