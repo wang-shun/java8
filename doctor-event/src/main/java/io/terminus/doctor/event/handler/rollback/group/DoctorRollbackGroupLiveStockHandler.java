5d6d16885f31c19be40138cb1a9cb1a5d1f1fd7b
DreamYoung <yangzhiliang1018@gmail.com>
2016-09-22 15:53:55 +0800

    add sample group rollback

--- doctor-event/src/main/java/io/terminus/doctor/event/handler/rollback/group/DoctorRollbackGroupLiveStockHandler.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/rollback/group/DoctorRollbackGroupLiveStockHandler.java
@@ -4,7 +4,6 @@ import io.terminus.doctor.event.dto.DoctorRollbackDto;
 import io.terminus.doctor.event.enums.GroupEventType;
 import io.terminus.doctor.event.handler.rollback.DoctorAbstractRollbackGroupEventHandler;
 import io.terminus.doctor.event.model.DoctorGroupEvent;
-import io.terminus.doctor.event.model.DoctorGroupSnapshot;
 import io.terminus.doctor.event.model.DoctorRevertLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,33 +12,23 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Desc: 新建猪群回滚
+ * Desc: 猪只存栏回滚
  * Mail: yangzl@terminus.io
  * author: DreamYoung
  * Date: 2016/9/22
  */
 @Slf4j
 @Component
-public class DoctorRollbackGroupNewEventHandler extends DoctorAbstractRollbackGroupEventHandler {
+public class DoctorRollbackGroupLiveStockHandler extends DoctorAbstractRollbackGroupEventHandler {
     @Override
     protected boolean handleCheck(DoctorGroupEvent groupEvent) {
-        //允许新建猪群事件回滚
-        return Objects.equals(groupEvent.getType(), GroupEventType.NEW.getValue());
+        //允许猪只存栏事件回滚
+        return Objects.equals(groupEvent.getType(), GroupEventType.LIVE_STOCK.getValue());
     }
 
     @Override
     protected DoctorRevertLog handleRollback(DoctorGroupEvent groupEvent) {
-        DoctorGroupSnapshot snapshot = doctorGroupSnapshotDao.findGroupSnapshotByToEventId(groupEvent.getId());
-
-        //删除此事件 -> 删除猪群跟踪 -> 删除猪群 -> 删除镜像
-        doctorGroupEventDao.delete(groupEvent.getId());
-        doctorGroupTrackDao.deleteByGroupId(groupEvent.getGroupId());
-        doctorGroupDao.delete(groupEvent.getGroupId());
-        doctorGroupSnapshotDao.delete(snapshot.getId());
-        return DoctorRevertLog.builder()
-                .fromInfo(snapshot.getToInfo())
-                .toInfo(snapshot.getFromInfo())
-                .build();
+        return super.sampleRollback(groupEvent);
     }
 
     @Override
