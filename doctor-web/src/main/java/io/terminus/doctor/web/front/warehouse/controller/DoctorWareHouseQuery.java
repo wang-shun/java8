dc4d0db49d0d30fde37282c4dc670c274a60785c
陈增辉 <zenghui@terminus.io>
2016-09-28 14:16:20 +0800

    fix npe of warehouseReport

--- doctor-web/src/main/java/io/terminus/doctor/web/front/warehouse/controller/DoctorWareHouseQuery.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/warehouse/controller/DoctorWareHouseQuery.java
@@ -303,9 +303,7 @@ public class DoctorWareHouseQuery {
                 farmId, null, WareHouseType.from(wareHouseType), null, startAt == null ? null : DateTime.parse(startAt).toDate(),
                 endAt == null ? null : DateTime.parse(endAt).plusDays(1).toDate()
         ));
-        if(warehouseEventReports.isEmpty()){
-            return new WarehouseReport();
-        }
+
         Map<Long, WarehouseReport.Report> reportmap = new HashMap<>();
         for(WarehouseEventReport report : warehouseEventReports){
             Long houseId = report.getWarehouseId();
