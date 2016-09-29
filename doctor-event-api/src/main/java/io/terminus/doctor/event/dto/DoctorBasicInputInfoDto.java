2b8c341a88560367b1475cee2a7a09401df9a8f3
xiaojiannan <xiaojiannan@terminus.io>
2016-09-29 20:38:25 +0800

    add isAuto

--- doctor-event-api/src/main/java/io/terminus/doctor/event/dto/DoctorBasicInputInfoDto.java
+++ doctor-event-api/src/main/java/io/terminus/doctor/event/dto/DoctorBasicInputInfoDto.java
@@ -86,6 +86,8 @@ public class DoctorBasicInputInfoDto implements Serializable{
 
     private Long relGroupEventId;
 
+    private Integer isAuto;
+
     /**
      * 相同宿舍, 不同pig构建
      * @param pigId
