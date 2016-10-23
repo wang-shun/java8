c640170a1a6afc1149df3c8f92c32da810dc4622
DreamYoung <yangzhiliang1018@gmail.com>
2016-10-23 11:57:15 +0800

    add name

--- doctor-move-data/src/main/java/io/terminus/doctor/move/controller/DoctorImportDataController.java
+++ doctor-move-data/src/main/java/io/terminus/doctor/move/controller/DoctorImportDataController.java
@@ -16,8 +16,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * Desc:
@@ -41,36 +44,36 @@ public class DoctorImportDataController {
 
     /**
      * 导入所有的猪场数据
-     * @param file excel文件
+     * @param path excel文件路径
      * @return 是否成功
      */
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public boolean importAll(MultipartFile file) {
+    public boolean importAll(@RequestParam("path") String path) {
         try {
             Workbook workbook;
-
+            File file = new File(path);
             if (file.getName().endsWith(".xlsx")){
-                workbook = new XSSFWorkbook(file.getInputStream());  //2007
-            }else if (file.getName().endsWith(".xls")){
-                workbook = new HSSFWorkbook(file.getInputStream());  //2003
+                workbook = new XSSFWorkbook(new FileInputStream(file));  //2007
+            } else if (file.getName().endsWith(".xls")){
+                workbook = new HSSFWorkbook(new FileInputStream(file));  //2003
             } else {
                 throw new ServiceException("file.type.error");
             }
 
             DoctorImportSheet sheet = new DoctorImportSheet();
-            sheet.setFarm(getSheet(workbook, ""));
-            sheet.setStaff(getSheet(workbook, ""));
-            sheet.setBarn(getSheet(workbook, ""));
-            sheet.setBreed(getSheet(workbook, ""));
-            sheet.setSow(getSheet(workbook, ""));
-            sheet.setBoar(getSheet(workbook, ""));
-            sheet.setGroup(getSheet(workbook, ""));
-            sheet.setWarehouse(getSheet(workbook, ""));
-            sheet.setMedicine(getSheet(workbook, ""));
-            sheet.setVacc(getSheet(workbook, ""));
-            sheet.setMaterial(getSheet(workbook, ""));
-            sheet.setFeed(getSheet(workbook, ""));
-            sheet.setConsume(getSheet(workbook, ""));
+            sheet.setFarm(getSheet(workbook, "猪场"));
+            sheet.setStaff(getSheet(workbook, "员工"));
+            sheet.setBarn(getSheet(workbook, "1.猪舍"));
+            sheet.setBreed(getSheet(workbook, "2.品种"));
+            sheet.setSow(getSheet(workbook, "3.母猪信息"));
+            sheet.setBoar(getSheet(workbook, "4.公猪信息"));
+            sheet.setGroup(getSheet(workbook, "5.商品猪（猪群）信息"));
+            sheet.setWarehouse(getSheet(workbook, "6.仓库"));
+            sheet.setMedicine(getSheet(workbook, "7.药品"));
+            sheet.setVacc(getSheet(workbook, "8.疫苗"));
+            sheet.setMaterial(getSheet(workbook, "9.原料"));
+            sheet.setFeed(getSheet(workbook, "10.饲料"));
+            sheet.setConsume(getSheet(workbook, "11.易耗品"));
             doctorImportDataService.importAll(sheet);
 
             log.warn("ElasticSearch dump start !");
@@ -81,10 +84,10 @@ public class DoctorImportDataController {
 
             return true;
         } catch (ServiceException e) {
-            log.error("import all excel failed, file:{}, cause:{}", file.getName(), Throwables.getStackTraceAsString(e));
+            log.error("import all excel failed, path:{}, cause:{}", path, Throwables.getStackTraceAsString(e));
             return false;
         } catch (Exception e) {
-            log.error("import all excel failed, file:{}, cause:{}", file.getName(), Throwables.getStackTraceAsString(e));
+            log.error("import all excel failed, path:{}, cause:{}", path, Throwables.getStackTraceAsString(e));
             return false;
         }
     }
