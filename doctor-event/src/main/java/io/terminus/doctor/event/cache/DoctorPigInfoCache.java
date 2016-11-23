e817ff816388fc84ff2995d34b5c83551c135b8d
xiaojiannan <xiaojiannan@terminus.io>
2016-11-23 15:04:05 +0800

    fix pigcode cache

--- doctor-event/src/main/java/io/terminus/doctor/event/cache/DoctorPigInfoCache.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/cache/DoctorPigInfoCache.java
@@ -31,7 +31,7 @@ public class DoctorPigInfoCache {
     public DoctorPigInfoCache(DoctorPigDao doctorPigDao){
         this.doctorPigDao = doctorPigDao;
 
-        pigCodeCache = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES).maximumSize(10000).build(new CacheLoader<Long, List<String>>() {
+        pigCodeCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.DAYS).maximumSize(10000).build(new CacheLoader<Long, List<String>>() {
             @Override
             public List<String> load(Long key) throws Exception {
                 return doctorPigDao.findPigCodesByCompanyId(key);
