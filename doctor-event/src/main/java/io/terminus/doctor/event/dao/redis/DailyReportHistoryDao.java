570adcdf34867974a63c315adea1a28ab3680e48
DreamYoung <yangzhiliang1018@gmail.com>
2016-11-29 17:55:54 +0800

    set uuid ot key

--- doctor-event/src/main/java/io/terminus/doctor/event/dao/redis/DailyReportHistoryDao.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/redis/DailyReportHistoryDao.java
@@ -64,10 +64,11 @@ public class DailyReportHistoryDao {
      */
     public void saveDailyReport(DoctorDailyReportDto reportDto, Long farmId, Date sumAt){
         log.info("save farmId:{}, sumAt:{}, DoctorDailyReportDto:{}", farmId, sumAt, reportDto);
-        String result = JsonMapper.JSON_NON_EMPTY_MAPPER.toJson(reportDto) + " ----- "+ UUID.randomUUID().toString();
+        String result = JsonMapper.JSON_NON_EMPTY_MAPPER.toJson(reportDto) + " +-----+ "+ UUID.randomUUID().toString();
         log.info("fucked result:{}", result);
         jedisTemplate.execute(jedis -> {
             jedis.setex(getRedisKey(farmId, sumAt), 86400, result + " ===== " + UUID.randomUUID().toString());
+            jedis.setex(UUID.randomUUID().toString(), 86400, result + " ===== " + UUID.randomUUID().toString());
         });
     }
 
