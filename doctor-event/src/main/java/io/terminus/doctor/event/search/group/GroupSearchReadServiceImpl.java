1acb5338851bdf79eca8e44618e6d9beb81edecd
DreamYoung <yangzhiliang1018@gmail.com>
2016-12-15 15:03:51 +0800

    add group qty

--- doctor-event/src/main/java/io/terminus/doctor/event/search/group/GroupSearchReadServiceImpl.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/search/group/GroupSearchReadServiceImpl.java
@@ -3,10 +3,11 @@ package io.terminus.doctor.event.search.group;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import io.terminus.boot.rpc.common.annotation.RpcProvider;
-import io.terminus.common.model.Paging;
 import io.terminus.common.model.Response;
 import io.terminus.doctor.common.enums.PigType;
+import io.terminus.doctor.common.utils.CountUtil;
 import io.terminus.doctor.event.search.barn.SearchedBarnDto;
+import io.terminus.doctor.event.search.query.GroupPaging;
 import io.terminus.search.api.Searcher;
 import io.terminus.search.api.model.WithAggregations;
 import io.terminus.search.api.query.Criterias;
@@ -66,7 +67,7 @@ public class GroupSearchReadServiceImpl implements GroupSearchReadService {
                     criterias,
                     SearchedGroup.class
             );
-            Paging<SearchedGroup> paging = new Paging<>(searchedGroups.getTotal(), searchedGroups.getData());
+            GroupPaging<SearchedGroup> paging = new GroupPaging<>(searchedGroups.getTotal(), searchedGroups.getData());
 
             // 获取聚合后的数据
             List<SearchedBarnDto.PigType> aggPigTypes = Lists.newArrayList();
@@ -76,6 +77,9 @@ public class GroupSearchReadServiceImpl implements GroupSearchReadService {
                 aggregations.get("aggs_pigType").forEach(bucket ->
                         aggPigTypes.add(SearchedBarnDto.createPigType(PigType.from(Integer.parseInt(bucket.getKey())), bucket.getDoc_count())));
             }
+
+            //set一发每次查询结果的猪群里猪的数量
+            paging.setCount(getGroupQty(template, params));
 
             SearchedGroupDto searchedGroupDto = SearchedGroupDto.builder()
                     .groups(paging)
@@ -87,5 +91,17 @@ public class GroupSearchReadServiceImpl implements GroupSearchReadService {
             return Response.fail("search.group.fail");
         }
 
+    }
+
+    private Long getGroupQty(String template, Map<String, String> params) {
+        Criterias criterias = baseGroupQueryBuilder.buildCriteriasWithoutPaging(params);
+        WithAggregations<SearchedGroup> searchedGroups = searcher.searchWithAggs(
+                groupSearchProperties.getIndexName(),
+                groupSearchProperties.getIndexType(),
+                template,
+                criterias,
+                SearchedGroup.class
+        );
+        return CountUtil.sumInt(searchedGroups.getData(), SearchedGroup::getQuantity);
     }
 }
