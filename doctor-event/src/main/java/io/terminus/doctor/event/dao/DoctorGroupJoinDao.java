104cd6fc0b3bb7a602c0fa88648845c8c59a424b
DreamYoung <yangzhiliang1018@gmail.com>
2016-12-23 18:44:23 +0800

    add get group count

---
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorGroupJoinDao.java
@@ -0,0 +1,20 @@
+package io.terminus.doctor.event.dao;
+
+import io.terminus.common.mysql.dao.MyBatisDao;
+import io.terminus.doctor.event.dto.DoctorGroupSearchDto;
+import io.terminus.doctor.event.search.group.SearchedGroup;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Desc: 猪群join
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 2016/12/22
+ */
+@Repository
+public class DoctorGroupJoinDao extends MyBatisDao<SearchedGroup> {
+
+    public Long getPigCount(DoctorGroupSearchDto groupSearchDto) {
+        return getSqlSession().selectOne(sqlId("getPigCount"), groupSearchDto);
+    }
+}
