5eb21170079720b463d1424482b8664b24390f25
DreamYoung <yangzhiliang1018@gmail.com>
2017-01-03 14:39:27 +0800

    remove unit test

--- doctor-basic/src/main/java/io/terminus/doctor/basic/DoctorBasicConfiguration.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/DoctorBasicConfiguration.java
@@ -1,17 +1,9 @@
 package io.terminus.doctor.basic;
 
-import io.terminus.doctor.basic.search.material.BaseMaterialQueryBuilder;
-import io.terminus.doctor.basic.search.material.DefaultIndexedMaterialFactory;
-import io.terminus.doctor.basic.search.material.DefaultMaterialQueryBuilder;
-import io.terminus.doctor.basic.search.material.IndexedMaterial;
-import io.terminus.doctor.basic.search.material.IndexedMaterialFactory;
-import io.terminus.doctor.basic.search.material.MaterialSearchProperties;
 import io.terminus.doctor.common.DoctorCommonConfiguration;
 import io.terminus.search.core.ESClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -39,23 +31,6 @@ public class DoctorBasicConfiguration {
         public ESClient esClient(@Value("${search.host:localhost}") String host,
                                  @Value("${search.port:9200}") Integer port) {
             return new ESClient(host, port);
-        }
-
-        @Configuration
-        @EnableConfigurationProperties(MaterialSearchProperties.class)
-        protected static class MaterialSearchConfiguration {
-
-            @Bean
-            @ConditionalOnMissingBean(IndexedMaterialFactory.class)
-            public IndexedMaterialFactory<? extends IndexedMaterial> indexedMaterialFactory() {
-                return new DefaultIndexedMaterialFactory();
-            }
-
-            @Bean
-            @ConditionalOnMissingBean(BaseMaterialQueryBuilder.class)
-            public BaseMaterialQueryBuilder baseMaterialQueryBuilder() {
-                return new DefaultMaterialQueryBuilder();
-            }
         }
     }
 
