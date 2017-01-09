7486318ac80f2d1b64a8ef5ebfadd25b09cff4b2
DreamYoung <yangzhiliang1018@gmail.com>
2017-01-09 13:49:31 +0800

    add publist dto

---
+++ doctor-event/src/main/java/io/terminus/doctor/event/event/DoctorPigPublishDto.java
@@ -0,0 +1,25 @@
+package io.terminus.doctor.event.event;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Desc: 批量事件时，发布事件需要的数据
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 2017/1/9
+ */
+@Data
+public class DoctorPigPublishDto implements Serializable {
+    private static final long serialVersionUID = -3312764468271438715L;
+
+    private Long pigId;
+
+    private Long eventId;
+
+    private Date eventAt;
+
+    private Integer eventType;
+}
--- doctor-event/src/main/java/io/terminus/doctor/event/event/ListenedGroupEvent.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/event/ListenedGroupEvent.java
@@ -6,9 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Builder;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
- * Created by xjn on 16/11/9.
+ * Desc: 猪群事件EventBus携带信息
+ * Mail: yangzl@terminus.io
+ * author: DreamYoung
+ * Date: 2016/11/30
  */
 @Data
 @Builder
@@ -16,8 +20,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class ListenedGroupEvent implements Serializable{
     private static final long serialVersionUID = 4293105794097951698L;
-    private Long groupId;
     private Long orgId;
+
     private Long farmId;
-    private Long doctorGroupEventId;
+
+    private List<DoctorGroupPublishDto> groups;
 }
