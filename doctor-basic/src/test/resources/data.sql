3e07ccd4226c57379c7fa86159450149ecff3368
luoys <luoys@terminus.io>
2017-01-11 18:26:24 +0800

    modify warehouse

---
+++ doctor-basic/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `doctor_material_price_in_ware_houses` (`id`, `farm_id`, `farm_name`, `ware_house_id`, `ware_house_name`, `material_id`, `material_name`, `type`, `provider_id`, `unit_price`, `remainder`, `provider_time`, `extra`, `creator_id`, `updator_id`, `created_at`, `updated_at`)
+VALUES
+	(130, 1, '融利实业种猪场', 5, '消耗品仓库', 375, '防疫服', 5, 390, 700, 157.000, '2012-04-11 11:03:15', NULL, NULL, NULL, '2016-10-18 10:27:34', '2016-12-14 19:27:40');
