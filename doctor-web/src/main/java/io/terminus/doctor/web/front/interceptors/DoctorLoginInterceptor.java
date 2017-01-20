ab607e38be36a63fdb279af79c16f038db094797
DreamYoung <yangzhiliang1018@gmail.com>
2017-01-20 17:57:30 +0800

    fix interceptor

--- doctor-web/src/main/java/io/terminus/doctor/web/front/interceptors/DoctorLoginInterceptor.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/interceptors/DoctorLoginInterceptor.java
@@ -10,7 +10,6 @@ import com.google.common.cache.LoadingCache;
 import io.terminus.common.model.Response;
 import io.terminus.doctor.user.util.DoctorUserMaker;
 import io.terminus.doctor.web.core.Constants;
-import io.terminus.doctor.web.core.login.Sessions;
 import io.terminus.pampas.common.UserUtil;
 import io.terminus.pampas.engine.common.WebUtil;
 import io.terminus.parana.common.model.ParanaUser;
@@ -25,11 +24,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-
-import static io.terminus.common.utils.Arguments.isEmpty;
 
 /**
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
@@ -56,11 +51,12 @@ public class DoctorLoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         WebUtil.putRequestAndResponse(request, response);
-        //兼容app壳里的h5和web！！！
+
         HttpSession session = request.getSession(false);
         if (session != null) {
             Object userIdInSession = session.getAttribute(Constants.SESSION_USER_ID);
             if (userIdInSession != null) {
+
                 final Long userId = Long.valueOf(userIdInSession.toString());
                 Response<? extends User> result = userCache.getUnchecked(userId);
                 if (!result.isSuccess()) {
@@ -72,41 +68,9 @@ public class DoctorLoginInterceptor extends HandlerInterceptorAdapter {
                 if (user != null) {
                     ParanaUser paranaUser = DoctorUserMaker.from(user);
                     UserUtil.putCurrentUser(paranaUser);
-                    return true;
                 }
             }
         }
-
-        if (request.getAttribute("sid") == null || isEmpty((String)request.getAttribute("sid"))) {
-            log.warn("session id miss");
-            return true;
-        }
-
-        String sessionId = request.getAttribute("sid").toString();
-        Map<String, Object> snapshot = sessionManager.findSessionById(Sessions.TOKEN_PREFIX, sessionId);
-        if (snapshot == null || snapshot.size() == 0 || snapshot.get(Sessions.USER_ID) == null) {
-            log.error("session expired sid:{}", sessionId);
-            return false;
-        }
-
-        //如果存在设备号，判断设备号是否一致
-        Object sessionDeviceId = snapshot.get(Sessions.DEVICE_ID);
-        Object requestDeviceId = request.getAttribute("deviceId");
-        if (sessionDeviceId != null && !Objects.equals(sessionDeviceId, requestDeviceId)) {
-            log.error("device id not match, sessionDeviceId:{}, requestDeviceId:{}", sessionDeviceId, requestDeviceId);
-            return false;
-        }
-
-        //刷新session过期时间
-        sessionManager.refreshExpireTime(Sessions.TOKEN_PREFIX, sessionId, Sessions.LONG_INACTIVE_INTERVAL);
-        Long uid = Long.parseLong(snapshot.get(Sessions.USER_ID).toString());
-        Response<User> res = userCache.getUnchecked(uid);
-        if (!res.isSuccess()) {
-            return false;
-        }
-
-        ParanaUser paranaUser = DoctorUserMaker.from(res.getResult());
-        UserUtil.putCurrentUser(paranaUser);
         return true;
     }
 
