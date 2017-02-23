a3c6dccc92cc9c92fecc22bc7135c97899e04353
xiaojiannan <xiaojiannan@terminus.io>
2017-02-23 13:41:00 +0800

    groupevent add sowcode

--- doctor-event/src/main/java/io/terminus/doctor/event/handler/group/DoctorCommonGroupEventHandler.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/group/DoctorCommonGroupEventHandler.java
@@ -105,6 +105,7 @@ public class DoctorCommonGroupEventHandler {
      */
     public void autoTransEventMoveIn(List<DoctorEventInfo> eventInfoList, DoctorGroup fromGroup, DoctorGroupTrack fromGroupTrack, DoctorTransGroupInput transGroup) {
         DoctorMoveInGroupInput moveIn = new DoctorMoveInGroupInput();
+        moveIn.setSowCode(transGroup.getSowCode());
         moveIn.setEventAt(transGroup.getEventAt());
         moveIn.setEventType(GroupEventType.MOVE_IN.getValue());
         moveIn.setIsAuto(IsOrNot.YES.getValue());
