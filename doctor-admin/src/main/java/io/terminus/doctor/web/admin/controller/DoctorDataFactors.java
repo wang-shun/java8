f882b00756bb00f09b9eb4dcb156b01c3e1b3ff2
hehaiyang <hehaiyang@terminus.io>
2017-04-27 14:44:20 +0800

    fix factors

--- doctor-admin/src/main/java/io/terminus/doctor/web/admin/controller/DoctorDataFactors.java
+++ doctor-admin/src/main/java/io/terminus/doctor/web/admin/controller/DoctorDataFactors.java
@@ -86,12 +86,12 @@ public class DoctorDataFactors {
             dto.setSubType(factor.getSubType());
             dto.setSubTypeName(factor.getSubTypeName());
             dto.setFactor(factor.getFactor());
-            if(factor.getRangeFrom() !=null && factor.getRangeFrom().equals(Double.MIN_VALUE)){
+            if("-1.79769313486232e308".equals(String.valueOf(factor.getRangeFrom()))){
                 dto.setRangeFrom("MIN");
             }else{
                 dto.setRangeFrom(String.valueOf(factor.getRangeFrom()));
             }
-            if(factor.getRangeTo() != null && factor.getRangeTo().equals(Double.MIN_VALUE)){
+            if("1.79769313486232e308".equals(String.valueOf(factor.getRangeFrom()))){
                 dto.setRangeTo("MAX");
             }else{
                 dto.setRangeTo(String.valueOf(factor.getRangeTo()));
