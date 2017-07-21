4e93a1ddc8797e40dead852ddf8415d6a7ff7bb9
xiaojiannan <xiaojiannan@terminus.io>
2017-07-21 10:22:34 +0800

    get group unwanqty

--- doctor-event/src/main/java/io/terminus/doctor/event/manager/DoctorGroupReportManager.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/manager/DoctorGroupReportManager.java
@@ -1,18 +1,17 @@
 package io.terminus.doctor.event.manager;
 
-import com.google.common.base.MoreObjects;
 import io.terminus.doctor.common.enums.PigType;
 import io.terminus.doctor.event.dao.DoctorGroupBatchSummaryDao;
+import io.terminus.doctor.event.dao.DoctorKpiDao;
 import io.terminus.doctor.event.dao.DoctorPigTrackDao;
 import io.terminus.doctor.event.model.DoctorGroupBatchSummary;
 import io.terminus.doctor.event.model.DoctorGroupTrack;
-import io.terminus.doctor.event.model.DoctorPigTrack;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -27,12 +26,15 @@ public class DoctorGroupReportManager {
 
     private final DoctorPigTrackDao doctorPigTrackDao;
     private final DoctorGroupBatchSummaryDao doctorGroupBatchSummaryDao;
+    private final DoctorKpiDao doctorKpiDao;
 
     @Autowired
     public DoctorGroupReportManager(DoctorPigTrackDao doctorPigTrackDao,
-                                    DoctorGroupBatchSummaryDao doctorGroupBatchSummaryDao) {
+                                    DoctorGroupBatchSummaryDao doctorGroupBatchSummaryDao,
+                                    DoctorKpiDao doctorKpiDao) {
         this.doctorPigTrackDao = doctorPigTrackDao;
         this.doctorGroupBatchSummaryDao = doctorGroupBatchSummaryDao;
+        this.doctorKpiDao = doctorKpiDao;
     }
 
     /**
@@ -45,22 +47,10 @@ public class DoctorGroupReportManager {
         if (!PigType.FARROW_TYPES.contains(pigType)) {
             return groupTrack;
         }
-
-        int weakQty = 0;
-        int unWeanQty = 0;
-        int unqQty = 0;
-
-        List<DoctorPigTrack> pigTracks = doctorPigTrackDao.findFeedSowTrackByGroupId(groupTrack.getGroupId());
-        for (DoctorPigTrack pigTrack : pigTracks) {
-            Map<String, Object> extraMap = pigTrack.getExtraMap();
-            weakQty += getIntFromExtra(extraMap, "weakCount");
-            unWeanQty += MoreObjects.firstNonNull(pigTrack.getUnweanQty(), 0);
-            unqQty += getIntFromExtra(extraMap, "qualifiedCount");
-        }
-
-        groupTrack.setWeakQty(weakQty);      //弱仔数
-        groupTrack.setUnweanQty(unWeanQty < groupTrack.getQuantity() ? unWeanQty : groupTrack.getQuantity());  //未断奶数
-        groupTrack.setUnqQty(unqQty);        //合格数
+        groupTrack.setWeakQty(0);      //弱仔数
+        groupTrack.setUnweanQty(doctorKpiDao.getGroupUnWean(groupTrack.getGroupId(), new Date()));  //未断奶数
+        groupTrack.setUnweanQty(doctorKpiDao.getGroupWean(groupTrack.getGroupId(), new Date()));  //未断奶数
+        groupTrack.setUnqQty(0);        //合格数
         return groupTrack;
     }
 
