eb7b1f11ae826f3bc04ed2aaba7ebca0e3fe2650
luomu32 <luomu32@hotmail.com>
2017-08-24 14:05:54 +0800

    添加仓库名称字段方便查询

--- doctor-basic-api/src/main/java/io/terminus/doctor/basic/model/warehouse/DoctorWarehouseMaterialHandle.java
+++ doctor-basic-api/src/main/java/io/terminus/doctor/basic/model/warehouse/DoctorWarehouseMaterialHandle.java
@@ -11,16 +11,16 @@ import java.util.Date;
 /**
  * Desc:
  * Mail: [ your email ]
- * Date: 2017-08-22 01:46:48
+ * Date: 2017-08-24 13:34:00
  * Created by [ your name ]
  */
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class DoctorWarehouseMaterialHandle implements Serializable {
 
-    private static final long serialVersionUID = -7044474816310444397L;
+    private static final long serialVersionUID = -760508433193325235L;
 
     /**
      * 自增主键
@@ -28,37 +28,42 @@ public class DoctorWarehouseMaterialHandle implements Serializable {
     private Long id;
     
     /**
-     * 
+     * 猪厂编号
      */
     private Long farmId;
     
     /**
-     * 
+     * 仓库编号
      */
     private Long warehouseId;
     
     /**
-     * 
+     * 仓库类型
      */
     private Integer warehouseType;
     
     /**
-     * 
+     * 仓库名称
+     */
+    private String warehouseName;
+    
+    /**
+     * 调拨，调入仓库编号
      */
     private Long targetWarehouseId;
     
     /**
-     * 
+     * 物料供应商名称
      */
     private String vendorName;
     
     /**
-     * 
+     * 物料编号
      */
     private Long materialId;
     
     /**
-     * 
+     * 物料名称
      */
     private String materialName;
     
@@ -68,27 +73,27 @@ public class DoctorWarehouseMaterialHandle implements Serializable {
     private Integer type;
     
     /**
-     * 
+     * 单价，单位分
      */
     private Long unitPrice;
     
     /**
-     * 
+     * 数量
      */
     private java.math.BigDecimal quantity;
     
     /**
-     * 
+     * 处理日期
      */
     private Date handleDate;
     
     /**
-     * 
+     * 处理年
      */
     private Integer handleYear;
     
     /**
-     * 
+     * 处理月
      */
     private Integer handleMonth;
     
