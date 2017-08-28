19dc7aa810203796703d2ec9649bec954107d97f
luomu32 <luomu32@hotmail.com>
2017-08-28 14:00:25 +0800

    整理重构仓库模块数据库脚本

--- db/db_change.sql
+++ db/db_change.sql
@@ -1279,30 +1279,7 @@ ALTER table doctor_daily_reports modify column `fatten_feed_amount` bigint(20) D
 
 --2017-08-28 仓库模块重构表结构
 
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: 127.0.0.1    Database: new_pig_doctor
--- ------------------------------------------------------
--- Server version	5.7.18
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `doctor_warehouse_material_apply`
---
-
 DROP TABLE IF EXISTS `doctor_warehouse_material_apply`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctor_warehouse_material_apply` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
   `material_handle_id` bigint(20) NOT NULL COMMENT '物料处理编号',
@@ -1329,15 +1306,7 @@ CREATE TABLE `doctor_warehouse_material_apply` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='仓库物料领用表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `doctor_warehouse_material_handle`
---
-
 DROP TABLE IF EXISTS `doctor_warehouse_material_handle`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctor_warehouse_material_handle` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
   `farm_id` bigint(20) DEFAULT NULL COMMENT '猪厂编号',
@@ -1361,15 +1330,7 @@ CREATE TABLE `doctor_warehouse_material_handle` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='仓库物料处理表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `doctor_warehouse_purchase`
---
-
 DROP TABLE IF EXISTS `doctor_warehouse_purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctor_warehouse_purchase` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
   `farm_id` bigint(20) DEFAULT NULL COMMENT '猪厂编号',
@@ -1390,15 +1351,7 @@ CREATE TABLE `doctor_warehouse_purchase` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='仓库物料入库表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `doctor_warehouse_stock`
---
-
 DROP TABLE IF EXISTS `doctor_warehouse_stock`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctor_warehouse_stock` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
   `warehouse_id` bigint(20) DEFAULT NULL COMMENT '仓库编号',
@@ -1415,17 +1368,5 @@ CREATE TABLE `doctor_warehouse_stock` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COMMENT='仓库物料库存表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-08-28 10:08:38
 
 
