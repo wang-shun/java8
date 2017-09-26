67430f1730528fa4a985759fd87f6ad97b3900d0
xiaojiannan <xiaojiannan@terminus.io>
2017-09-26 15:34:45 +0800

    import date error message

--- doctor-move-data/src/main/java/io/terminus/doctor/move/tools/DoctorImportValidator.java
+++ doctor-move-data/src/main/java/io/terminus/doctor/move/tools/DoctorImportValidator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import static io.terminus.common.utils.Arguments.notNull;
 import static io.terminus.doctor.common.utils.Checks.expectTrue;
+import static io.terminus.doctor.move.tools.DoctorMessageConverter.assembleErrorAttach;
 
 /**
  * Created by xjn on 17/9/4.
@@ -16,13 +17,15 @@ public class DoctorImportValidator {
     public DoctorImportSow valid(DoctorImportSow importSow) {
 
         if (notNull(importSow.getPregCheckDate())) {
-            expectTrue(!importSow.getPregCheckDate().before(importSow.getMateDate()), false
-                    ,importSow.getLineNumber().toString(), "pregCheckDate.before.mateDate", importSow.getSowCode());
+            expectTrue(!importSow.getPregCheckDate().before(importSow.getMateDate()), false,
+                    assembleErrorAttach(null, importSow.getLineNumber()),
+                    "pregCheckDate.before.mateDate");
         }
 
         if (notNull(importSow.getPregDate())) {
-            expectTrue(!importSow.getPregDate().before(importSow.getPregCheckDate()), false
-                    , importSow.getLineNumber().toString(), "pregDate.before.pregCheckDate",  importSow.getSowCode());
+            expectTrue(!importSow.getPregDate().before(importSow.getPregCheckDate()), false,
+                    assembleErrorAttach(null, importSow.getLineNumber()),
+                    "pregDate.before.pregCheckDate");
         }
 
         // TODO: 17/9/4 字段不为空校验 暂时没时间写以后再优化
