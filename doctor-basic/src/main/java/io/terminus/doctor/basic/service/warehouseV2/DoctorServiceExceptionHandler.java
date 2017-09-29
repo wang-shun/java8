2ef8d079a4604ad2d05a9ad49546c7ff9023acfa
luomu32 <luomu32@hotmail.com>
2017-09-29 17:58:49 +0800

    增加invliadexception的处理

--- doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorServiceExceptionHandler.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorServiceExceptionHandler.java
@@ -3,6 +3,7 @@ package io.terminus.doctor.basic.service.warehouseV2;
 import com.google.common.base.Throwables;
 import io.terminus.common.exception.ServiceException;
 import io.terminus.common.model.Response;
+import io.terminus.doctor.common.exception.InvalidException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -12,12 +13,15 @@ import org.aspectj.lang.reflect.AdviceSignature;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.lang.reflect.Method;
+import java.util.Locale;
 
 /**
  * Created by sunbo@terminus.io on 2017/9/27.
@@ -28,6 +32,8 @@ import java.lang.reflect.Method;
 @Order(300)//需要包裹在事务切面之外
 public class DoctorServiceExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
 
     @Around("execution(* io.terminus.doctor.basic.service.warehouseV2.*.*(..))")
     public Object exceptionHandle(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -43,6 +49,10 @@ public class DoctorServiceExceptionHandler {
 
                     if (throwable instanceof ServiceException)
                         return Response.fail(throwable.getMessage());
+                    if (throwable instanceof InvalidException) {
+                        InvalidException invalidException = (InvalidException) throwable;
+                        return Response.fail(messageSource.getMessage(invalidException.getError(), invalidException.getParams(), Locale.CHINA));
+                    }
 
                     log.error("{}, cause:{}", exceptionHandle.value(), Throwables.getStackTraceAsString(throwable));
                     return Response.fail(exceptionHandle.value());
