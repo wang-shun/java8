793410247bb64b2848b4c842c564278403ec310e
xiaojiannan <xiaojiannan@terminus.io>
2017-10-12 10:51:32 +0800

    add iotUserRoles controller

--- doctor-web/src/main/java/io/terminus/doctor/web/DoctorWebConfiguration.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/DoctorWebConfiguration.java
@@ -7,10 +7,6 @@ package io.terminus.doctor.web;
 import io.terminus.doctor.user.service.SubRoleReadService;
 import io.terminus.doctor.web.core.DoctorCoreWebConfiguration;
 import io.terminus.doctor.web.core.advices.JsonExceptionResolver;
-import io.terminus.doctor.web.core.export.ColumnFormatterRegistry;
-import io.terminus.doctor.web.core.export.DefaultExporter;
-import io.terminus.doctor.web.core.export.Exporter;
-import io.terminus.doctor.web.core.export.property.ExportTables;
 import io.terminus.doctor.web.core.msg.email.CommonEmailServiceConfig;
 import io.terminus.doctor.web.core.msg.sms.LuoSiMaoSmsServiceConfig;
 import io.terminus.doctor.web.core.service.OtherSystemServiceConfig;
@@ -27,11 +23,19 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
  * Date: 2016-02-01
  */
+@EnableSwagger2
 @Configuration
 @ComponentScan(basePackages = {
         "io.terminus.doctor.web.core.component",
@@ -62,4 +66,19 @@ public class DoctorWebConfiguration extends WebMvcConfigurerAdapter {
         return configurer;
     }
 
+    @Bean
+    public Docket config() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.terminus.doctor.web.front.role"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("pigDoctor系统API文档")
+                .build();
+    }
 }
---
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/role/IotUserRoles.java
@@ -0,0 +1,82 @@
+package io.terminus.doctor.web.front.role;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.terminus.boot.rpc.common.annotation.RpcConsumer;
+import io.terminus.common.model.Paging;
+import io.terminus.doctor.user.dto.IotUserRoleInfo;
+import io.terminus.doctor.user.model.IotRole;
+import io.terminus.doctor.user.service.IotUserRoleReadService;
+import io.terminus.doctor.user.service.IotUserRoleWriteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by xjn on 17/10/12.
+ */
+@Api("物联网角色操作")
+@Slf4j
+@RestController
+@RequestMapping("/iot/user/role")
+public class IotUserRoles {
+
+    @RpcConsumer
+    private IotUserRoleReadService roleReadService;
+    @RpcConsumer
+    private IotUserRoleWriteService roleWriteService;
+
+    /**
+     * 分页查询用户与物联网角色
+     * @param realName 用户真实姓名
+     * @param pageNo 页码
+     * @param pageSize 页大小
+     * @return 分页结果
+     */
+    @ApiOperation("分页查询用户与物联网角色")
+    @RequestMapping(value = "/paging/userRole", method = RequestMethod.GET)
+    public Paging<IotUserRoleInfo> pagingUserRole(@RequestParam(required = false) @ApiParam("用户真实姓名") String realName,
+                                                  @RequestParam @ApiParam("页码") Integer pageNo,
+                                                  @RequestParam @ApiParam("页大小") Integer pageSize) {
+        return null;
+    }
+
+    /**
+     * 创建或更新用户与角色关系
+     * @param iotUserRole 用户与角色关系
+     * @return 是否成功
+     */
+    @ApiOperation("创建或更新用户与角色关系")
+    @RequestMapping(value = "/createOrUpdate/iotUserRole", method = RequestMethod.POST)
+    public Boolean createOrUpdateIotUserRole(@RequestBody @ApiParam("用户与角色关系") IotUserRoles iotUserRole) {
+        return null;
+    }
+
+    /**
+     * 列出所有有效的角色
+     * @return 所有有效角色列表
+     */
+    @ApiOperation("列出所有有效的角色")
+    @RequestMapping(value = "/list/effectedRole", method = RequestMethod.GET)
+    public List<IotRole> listEffectedRole() {
+        return null;
+    }
+
+    /**
+     * 创建或更新物联网角色
+     * @param iotRole 物联网角色
+     * @return 是否成功
+     */
+    @ApiOperation("创建或更新物联网角色")
+    @RequestMapping(value = "/createOrUpdate/iotRole", method = RequestMethod.POST)
+    public Boolean createOrUpdateIotRole(@RequestBody @ApiParam("物联网角色") IotRole iotRole){
+        return null;
+    }
+
+}
