a6a97bce93462b2a7e38b55e3ba4f0ba2c606f42
xiaojiannan <xiaojiannan@terminus.io>
2017-10-16 11:18:26 +0800

    fix findIotUserRoleById

--- doctor-user/src/main/java/io/terminus/doctor/user/service/IotUserRoleReadServiceImpl.java
+++ doctor-user/src/main/java/io/terminus/doctor/user/service/IotUserRoleReadServiceImpl.java
@@ -4,18 +4,23 @@ import com.google.common.base.Throwables;
 import io.terminus.boot.rpc.common.annotation.RpcProvider;
 import io.terminus.common.model.Paging;
 import io.terminus.common.model.Response;
+import io.terminus.common.utils.BeanMapper;
 import io.terminus.doctor.user.dao.IotRoleDao;
 import io.terminus.doctor.user.dao.IotUserRoleDao;
 import io.terminus.doctor.user.dto.IotUserRoleInfo;
 import io.terminus.doctor.user.model.IotRole;
 import io.terminus.doctor.user.model.IotUserRole;
+import io.terminus.parana.user.impl.dao.UserDao;
 import io.terminus.parana.user.impl.dao.UserProfileDao;
+import io.terminus.parana.user.model.User;
 import io.terminus.parana.user.model.UserProfile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static io.terminus.common.utils.Arguments.notNull;
 
 /**
  * Created by xjn on 17/10/11.
@@ -27,12 +32,15 @@ public class IotUserRoleReadServiceImpl implements IotUserRoleReadService{
     private final IotUserRoleDao iotUserRoleDao;
     private final IotRoleDao iotRoleDao;
     private final UserProfileDao userProfileDao;
+    private final UserDao userDao;
 
     @Autowired
-    public IotUserRoleReadServiceImpl(IotUserRoleDao iotUserRoleDao, IotRoleDao iotRoleDao, UserProfileDao userProfileDao) {
+    public IotUserRoleReadServiceImpl(IotUserRoleDao iotUserRoleDao, IotRoleDao iotRoleDao,
+                                      UserProfileDao userProfileDao, UserDao userDao) {
         this.iotUserRoleDao = iotUserRoleDao;
         this.iotRoleDao = iotRoleDao;
         this.userProfileDao = userProfileDao;
+        this.userDao = userDao;
     }
 
     @Override
@@ -70,7 +78,16 @@ public class IotUserRoleReadServiceImpl implements IotUserRoleReadService{
         try {
             IotUserRole iotUserRole = iotUserRoleDao.findById(id);
             UserProfile userProfile = userProfileDao.findByUserId(iotUserRole.getUserId());
-            return null;
+            String name;
+            if (notNull(userProfile)) {
+                name = userProfile.getRealName();
+            } else {
+                User user = userDao.findById(iotUserRole.getUserId());
+                name = user.getName();
+            }
+            IotUserRoleInfo info = BeanMapper.map(iotUserRole, IotUserRoleInfo.class);
+            info.setRealName(name);
+            return Response.ok(info);
         } catch (Exception e) {
             log.error("find iot user role by id failed, id:{},cause:{}",
                     id, Throwables.getStackTraceAsString(e));
