e0c7682dfe05d8bcae4cd06637b75e8cdcdd33bc
xiaojiannan <xiaojiannan@terminus.io>
2017-12-11 17:36:46 +0800

    add doctorStatisticDao and doctorStatisticMapper

---
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorGroupDailyDao.java
@@ -0,0 +1,13 @@
+package io.terminus.doctor.event.dao;
+
+import io.terminus.common.mysql.dao.MyBatisDao;
+import io.terminus.doctor.event.model.DoctorGroupDaily;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by xjn on 17/12/11.
+ * email:xiaojiannan@terminus.io
+ */
+@Repository
+public class DoctorGroupDailyDao extends MyBatisDao<DoctorGroupDaily> {
+}
