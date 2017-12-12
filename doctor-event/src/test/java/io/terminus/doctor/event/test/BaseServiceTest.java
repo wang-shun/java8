8ea85022cde37f03a63ea0efc0229e832f344122
xiaojiannan <xiaojiannan@terminus.io>
2017-12-12 15:25:23 +0800

    add flush group daily test

--- doctor-event/src/test/java/io/terminus/doctor/event/test/BaseServiceTest.java
+++ doctor-event/src/test/java/io/terminus/doctor/event/test/BaseServiceTest.java
@@ -2,10 +2,8 @@ package io.terminus.doctor.event.test;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Desc: 工作流基础测试类
@@ -15,8 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(ServiceConfiguration.class)
-@Transactional
-@Rollback
+//@Transactional
+//@Rollback
 @ActiveProfiles("test")
 public class BaseServiceTest {
 
