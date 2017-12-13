5df130f1110795f6b50c36f12163ebb02647225b
luomu32 <luomu32@hotmail.com>
2017-12-13 14:30:28 +0800

    猪表报添加根据猪场或公司获取的方法

--- doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigDailyDao.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigDailyDao.java
@@ -3,8 +3,12 @@ package io.terminus.doctor.event.dao;
 import io.terminus.common.mysql.dao.MyBatisDao;
 import io.terminus.doctor.event.model.DoctorPigDaily;
 import org.springframework.stereotype.Repository;
+import sun.font.SunFontManager;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Desc:
@@ -15,6 +19,18 @@ import java.util.Date;
 @Repository
 public class DoctorPigDailyDao extends MyBatisDao<DoctorPigDaily> {
 
+
+    public DoctorPigDaily findByFarm(Long farmId) {
+        List<DoctorPigDaily> reports = this.list(Collections.singletonMap("farmId", farmId));
+        if (reports.isEmpty())
+            return null;
+        return reports.get(0);
+    }
+
+
+    public List<DoctorPigDaily> findByOrg(List<Long> farmIds) {
+        return this.list(Collections.singletonMap("farmIds", farmIds));
+    }
 
 
 }
