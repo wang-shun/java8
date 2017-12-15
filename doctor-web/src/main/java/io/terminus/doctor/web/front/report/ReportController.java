05a491061409697683cd498c99c9ade716e01b13
luomu32 <luomu32@hotmail.com>
2017-12-15 10:13:05 +0800

    添加报表controller

---
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/report/ReportController.java
@@ -0,0 +1,19 @@
+package io.terminus.doctor.web.front.report;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sunbo@terminus.io on 2017/12/13.
+ */
+@RestController
+@RequestMapping("/api/doctor/report")
+public class ReportController {
+
+
+
+    
+
+
+
+}
