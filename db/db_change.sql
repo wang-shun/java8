419478472d945ed90d3ef33621de4a6df9f551c2
xiaojiannan <xiaojiannan@terminus.io>
2017-12-20 16:34:53 +0800

    doctorPig add origin

--- db/db_change.sql
+++ db/db_change.sql
@@ -1709,6 +1709,8 @@ create table doctor_pig_dailies (
   KEY `idx_doctor_pig_dailies_farm_id` (`farm_id`),
   KEY `idx_doctor_pig_dailies_sum_at` (`sum_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='猪相关报表';
--- 猪 猪群原值
+-- 猪 猪群原值 2017-12-20
+>>>>>>> doctorPig add origin
 ALTER TABLE `doctor_pig_events` ADD COLUMN `origin` DOUBLE DEFAULT NULL COMMENT '原值' after `remark`;
 ALTER TABLE `doctor_group_events` ADD COLUMN `origin` DOUBLE DEFAULT NULL COMMENT '原值' after `remark`;
+ALTER TABLE `doctor_pigs` ADD COLUMN `origin` DOUBLE DEFAULT NULL COMMENT '原值' after `pig_code`;
