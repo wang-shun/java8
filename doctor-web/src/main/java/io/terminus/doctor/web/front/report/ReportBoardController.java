e5317cadaec75b3d656180dae0a21bf15c3b5f5e
xiao <xiao@xiaodeMacBook-Pro.local>
2018-01-18 19:47:22 +0800

    物料与效率指标错误

--- doctor-web/src/main/java/io/terminus/doctor/web/front/report/ReportBoardController.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/report/ReportBoardController.java
@@ -142,15 +142,16 @@ public class ReportBoardController {
                 case FATTEN: subField(doctorReportFieldTypeDto, report.getReportFatten()); break;
                 case BOAR: subField(doctorReportFieldTypeDto, report.getReportBoar()); break;
                 case MATERIAL:
-                    // TODO: 18/1/18 物料 
+                    // TODO: 18/1/18 物料数据错误太多
 //                    subField(doctorReportFieldTypeDto, report.getReportMaterial());
                     break;
                 case EFFICIENCY:
-                    if (DateDimension.YEARLY.contains(dimensionCriteria.getDateType())) {
-                        subField(doctorReportFieldTypeDto, report.getReportEfficiency());
-                    } else {
-                        subFieldDefault(doctorReportFieldTypeDto);
-                    }
+                    // TODO: 18/1/18 效率指标数据错误太多
+//                    if (DateDimension.YEARLY.contains(dimensionCriteria.getDateType())) {
+//                        subField(doctorReportFieldTypeDto, report.getReportEfficiency());
+//                    } else {
+//                        subFieldDefault(doctorReportFieldTypeDto);
+//                    }
                     break;
                 default:
                     throw new JsonResponseException("region.is.illegal");
