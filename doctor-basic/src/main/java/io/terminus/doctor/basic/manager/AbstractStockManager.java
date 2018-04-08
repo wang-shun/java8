2a2495bee1c8c8efda14552571328e1533c292f9
luomu32 <luomu32@hotmail.com>
2018-04-08 09:20:40 +0800

    add warehouse flow diagram

---
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/manager/AbstractStockManager.java
@@ -0,0 +1,15 @@
+package io.terminus.doctor.basic.manager;
+
+/**
+ * Created by sunbo@terminus.io on 2018/4/8.
+ */
+public abstract class AbstractStockManager {
+
+
+    /**
+     * 重算
+     */
+    protected void recalculate() {
+
+    }
+}
