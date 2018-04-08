5d4faa049e03b133b4dc8176680a1b33122d035e
xiao <xiao@xiaodeMacBook-Pro.local>
2018-04-08 17:51:44 +0800

    orgDayStock method add pigType arg

--- doctor-event/src/main/java/io/terminus/doctor/event/reportBi/DoctorReportBiDataSynchronize.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/reportBi/DoctorReportBiDataSynchronize.java
@@ -480,8 +480,8 @@ public class DoctorReportBiDataSynchronize {
             } else {
                 Date minDate = doctorGroupDailyDao.minDate(dimensionCriteria);
                 Date maxDate = doctorGroupDailyDao.maxDate(dimensionCriteria);
-                start = doctorGroupDailyDao.orgDayStock(dimensionCriteria.getOrzId(), minDate);
-                end = doctorGroupDailyDao.orgDayStock(dimensionCriteria.getOrzId(), maxDate);
+                start = doctorGroupDailyDao.orgDayStock(dimensionCriteria.getOrzId(), minDate, dimensionCriteria.getPigType());
+                end = doctorGroupDailyDao.orgDayStock(dimensionCriteria.getOrzId(), maxDate, dimensionCriteria.getPigType());
                 groupDaily.setDailyLivestockOnHand(FieldHelper.getInteger(doctorGroupDailyDao.orgDayAvgLiveStock(dimensionCriteria), DateUtil.getDeltaDaysAbs(maxDate, minDate)));
             }
             groupDaily.setStart(start);
