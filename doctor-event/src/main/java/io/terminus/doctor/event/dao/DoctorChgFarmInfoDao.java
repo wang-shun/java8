863402d9460fcd7205c818bd969517359e7d1009
肖建男 <xiaojiannan@terminus.io>
2018-05-07 12:01:14 +0800

    转场后修改耳号的，原场耳号信息也同步最新的耳号

--- doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorChgFarmInfoDao.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorChgFarmInfoDao.java
@@ -5,6 +5,8 @@ import io.terminus.common.mysql.dao.MyBatisDao;
 import io.terminus.doctor.event.model.DoctorChgFarmInfo;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by xjn on 18/4/20.
  * email:xiaojiannan@terminus.io
@@ -13,5 +15,9 @@ import org.springframework.stereotype.Repository;
 public class DoctorChgFarmInfoDao extends MyBatisDao<DoctorChgFarmInfo>{
     public DoctorChgFarmInfo findByFarmIdAndPigId(Long farmId, Long pigId) {
         return getSqlSession().selectOne(sqlId("findByFarmIdAndPigId"), ImmutableMap.of("farmId", farmId, "pigId", pigId));
+    }
+
+    public List<DoctorChgFarmInfo> findByPigId(Long pigId) {
+        return getSqlSession().selectList(sqlId("findByPigId"), pigId);
     }
 }
