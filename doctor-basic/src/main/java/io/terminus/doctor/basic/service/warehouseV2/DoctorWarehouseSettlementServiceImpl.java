7953893e04f7f46f3b56726eb1564cfab994799e
luomu32 <luomu32@hotmail.com>
2018-05-16 14:39:07 +0800

    add valid for find formula out stock handle when settlement

--- doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
@@ -222,10 +222,10 @@ public class DoctorWarehouseSettlementServiceImpl implements DoctorWarehouseSett
                 if (null == formulaInStockHandle)
                     throw new ServiceException("stock.handle.not.found");
                 //根据配方入库单据获取对应配方出库单据，再根据配方出库单据获取明细
-                log.info("find formula out stock handle for in stock handle :{}", formulaInStockHandle.getId());
-
                 List<Long> formulaOutStockHandleIds = doctorWarehouseStockHandleDao.findRelStockHandle(formulaInStockHandle.getId()).stream().map(DoctorWarehouseStockHandle::getId).collect(Collectors.toList());
-                log.info("formula out stock handle is {}", formulaOutStockHandleIds);
+                if (formulaOutStockHandleIds.isEmpty())
+                    throw new ServiceException("formula.out.stock.handle.not.found");
+
                 List<DoctorWarehouseMaterialHandle> formulaOutMaterialHandles = doctorWarehouseMaterialHandleDao.findByStockHandles(formulaOutStockHandleIds);
                 if (formulaOutMaterialHandles.isEmpty())
                     throw new ServiceException("formula.out.material.handle.not.found");
