dd48d2596fe19cdc7f9ae14824f4b40869c917d5
luomu32 <luomu32@hotmail.com>
2018-05-16 14:09:14 +0800

    add error for settlement when no formula out material handle found

--- doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
@@ -223,6 +223,8 @@ public class DoctorWarehouseSettlementServiceImpl implements DoctorWarehouseSett
                     throw new ServiceException("stock.handle.not.found");
                 //根据配方入库单据获取对应配方出库单据，再根据配方出库单据获取明细
                 List<DoctorWarehouseMaterialHandle> formulaOutMaterialHandles = doctorWarehouseMaterialHandleDao.findByStockHandles(doctorWarehouseStockHandleDao.findRelStockHandle(formulaInStockHandle.getId()).stream().map(DoctorWarehouseStockHandle::getId).collect(Collectors.toList()));
+                if (formulaOutMaterialHandles.isEmpty())
+                    throw new ServiceException("formula.out.material.handle.not.found");
 
                 BigDecimal totalFormulaOutAmount = formulaOutMaterialHandles
                         .stream()
