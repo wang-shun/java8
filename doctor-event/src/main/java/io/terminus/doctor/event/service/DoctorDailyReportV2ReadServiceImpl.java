4c05780e4c63730d9bde685a6d13327cb41e1bfd
肖建男 <xiaojiannan@terminus.io>
2018-05-16 10:22:01 +0800

    报表数据查询实现

--- doctor-event/src/main/java/io/terminus/doctor/event/service/DoctorDailyReportV2ReadServiceImpl.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/service/DoctorDailyReportV2ReadServiceImpl.java
@@ -49,41 +49,82 @@ public class DoctorDailyReportV2ReadServiceImpl implements DoctorDailyReportV2Re
 
     @Override
     public Response<List<DoctorReportBoar>> boarReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findBoarReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("boar report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("boar.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportDeliver>> deliverReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findDeliverReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("deliver report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("deliver.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportEfficiency>> efficiencyReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findEfficiencyReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("efficiency report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("efficiency.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportFatten>> fattenReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+
+        try {
+            return Response.ok(query.findFattenReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("fatten report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("fatten.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportMaterial>> materialReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findMaterialReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("material report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("material.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportMating>> matingReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findMatingReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("mating report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("mating.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportNursery>> nurseryReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findNurseryReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("nursery report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("nursery.report.failed");
+        }
     }
 
     @Override
     public Response<List<DoctorReportReserve>> reserveReport(DoctorDimensionCriteria dimensionCriteria) {
-        return null;
+        try {
+            return Response.ok(query.findReserveReportBy(dimensionCriteria));
+        } catch (Exception e) {
+            log.error("reserve report failed,cause:{}", Throwables.getStackTraceAsString(e));
+            return Response.fail("reserve.report.failed");
+        }
     }
 }
--- doctor-event/src/test/java/io/terminus/doctor/event/service/DoctorDailyReportV2ReadServiceTest.java
+++ doctor-event/src/test/java/io/terminus/doctor/event/service/DoctorDailyReportV2ReadServiceTest.java
@@ -1,13 +1,23 @@
 package io.terminus.doctor.event.service;
 
 import io.terminus.common.model.Response;
+import io.terminus.doctor.common.enums.IsOrNot;
 import io.terminus.doctor.common.utils.DateUtil;
 import io.terminus.doctor.event.dto.DoctorDimensionCriteria;
 import io.terminus.doctor.event.enums.DateDimension;
 import io.terminus.doctor.event.enums.OrzDimension;
+import io.terminus.doctor.event.model.DoctorReportBoar;
+import io.terminus.doctor.event.model.DoctorReportDeliver;
+import io.terminus.doctor.event.model.DoctorReportEfficiency;
+import io.terminus.doctor.event.model.DoctorReportFatten;
+import io.terminus.doctor.event.model.DoctorReportMating;
+import io.terminus.doctor.event.model.DoctorReportNursery;
+import io.terminus.doctor.event.model.DoctorReportReserve;
 import io.terminus.doctor.event.model.DoctorReportSow;
 import io.terminus.doctor.event.test.BaseServiceTest;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,16 +32,78 @@ public class DoctorDailyReportV2ReadServiceTest extends BaseServiceTest {
 
     @Autowired
     private DoctorDailyReportV2ReadService doctorDailyReportV2ReadService;
+    private  DoctorDimensionCriteria doctorDimensionCriteria;
 
-    @Test
-    public void findSowReportTest() {
-        DoctorDimensionCriteria doctorDimensionCriteria = new DoctorDimensionCriteria();
+    @Before
+    public void criteria() {
+        doctorDimensionCriteria = new DoctorDimensionCriteria();
         doctorDimensionCriteria.setOrzId(1L);
         doctorDimensionCriteria.setOrzType(OrzDimension.FARM.getValue());
         doctorDimensionCriteria.setStartAt(DateUtil.toDate("2018-01-01"));
         doctorDimensionCriteria.setEndAt(DateUtil.toDate("2018-02-01"));
         doctorDimensionCriteria.setDateType(DateDimension.DAY.getValue());
+        doctorDimensionCriteria.setIsNecessaryTotal(IsOrNot.YES.getKey());
+    }
+
+    @Test
+    public void findSowReportTest() {
         Response<List<DoctorReportSow>> doctorReportSowResponse = doctorDailyReportV2ReadService.sowReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findBoarReportTest() {
+        Response<List<DoctorReportBoar>> doctorReportSowResponse = doctorDailyReportV2ReadService.boarReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findDeliverReportTest() {
+        Response<List<DoctorReportDeliver>> doctorReportSowResponse = doctorDailyReportV2ReadService.deliverReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findMatingReportTest() {
+        Response<List<DoctorReportMating>> doctorReportSowResponse = doctorDailyReportV2ReadService.matingReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findFattenReportTest() {
+        Response<List<DoctorReportFatten>> doctorReportSowResponse = doctorDailyReportV2ReadService.fattenReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findNurseryReportTest() {
+        Response<List<DoctorReportNursery>> doctorReportSowResponse = doctorDailyReportV2ReadService.nurseryReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findReserveReportTest() {
+        Response<List<DoctorReportReserve>> doctorReportSowResponse = doctorDailyReportV2ReadService.reserveReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findMaterialReportTest() {
+        Response<List<DoctorReportReserve>> doctorReportSowResponse = doctorDailyReportV2ReadService.reserveReport(doctorDimensionCriteria);
+        Assert.assertTrue(doctorReportSowResponse.isSuccess());
+        System.out.println(doctorReportSowResponse.getResult().size());
+    }
+
+    @Test
+    public void findEfficiencyReportTest() {
+        Response<List<DoctorReportEfficiency>> doctorReportSowResponse = doctorDailyReportV2ReadService.efficiencyReport(doctorDimensionCriteria);
         Assert.assertTrue(doctorReportSowResponse.isSuccess());
         System.out.println(doctorReportSowResponse.getResult().size());
     }
