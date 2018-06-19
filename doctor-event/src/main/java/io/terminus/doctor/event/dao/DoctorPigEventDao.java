bec48d2a86e351fbf2207853f2d6ffb0cdc40676
kongjingjun <kongjj@fominfo.com>
2018-06-19 16:34:59 +0800

    'kjj'

--- doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigEventDao.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigEventDao.java
@@ -35,7 +35,7 @@ public class DoctorPigEventDao extends MyBatisDao<DoctorPigEvent> {
     }
 
     public List<DoctorPigEvent> getweansum(Map<String, Object> criteria){
-        return getSqlSession().selectList(sqlId("getweansum"),criteria);
+        return getSqlSession().selectList(sqlId("getabosum"),criteria);
     }
 
     public DoctorPigEvent findByRelGroupEventId(Long relGroupEventId) {
