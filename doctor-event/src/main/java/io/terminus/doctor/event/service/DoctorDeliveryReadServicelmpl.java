718ac803ebdc553e290994951c314f4f6aafc32b
kongjingjun <kongjj@fominfo.com>
2018-06-27 18:20:21 +0800

    ;kjjk

--- doctor-event/src/main/java/io/terminus/doctor/event/service/DoctorDeliveryReadServicelmpl.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/service/DoctorDeliveryReadServicelmpl.java
@@ -32,7 +32,7 @@ public class DoctorDeliveryReadServicelmpl implements DoctorDeliveryReadService{
             List<Map<String,Object>> deliveryBarn = doctorReportDeliverDao.deliveryBarn((BigInteger)map.get("pig_id"), (Date)map.get("event_at"));
             if(deliveryBarn.size() != 0){
                 map.put("deliveryBarn",(String)deliveryBarn.get(0).get("barn_name"));
-                map.put("deliveryDate",(Date)deliveryBarn.get(0).get("evenet_at"));
+                map.put("deliveryDate",(Date)deliveryBarn.get(0).get("event_at"));
             } else{
                 map.put("deliveryBarn","");
                 map.put("deliveryDate","");
