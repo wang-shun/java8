af5c6caf973f2a62b6d7754b314fdb970617c20b
kongjingjun <kongjj@fominfo.com>
2018-07-03 10:24:09 +0800

    'b'

--- doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseSettlementServiceImpl.java
@@ -113,21 +113,15 @@ public class DoctorWarehouseSettlementServiceImpl implements DoctorWarehouseSett
         try {
             DoctorWarehouseOrgSettlement settlement = doctorWarehouseOrgSettlementDao.findByOrg(orgId);
             if (null != settlement) {
-<<<<<<< HEAD
-                if (settlementDate.after(DateUtils.addMonths(settlement.getLastSettlementDate(), 1))) {
-=======
+
                 log.error("settlement:"+DateUtils.addDays(settlement.getLastSettlementDate(), 1).toGMTString());
                 log.error("settlementDate:"+settlementDate.toGMTString());
                 if (settlementDate.after(DateUtils.addDays(settlement.getLastSettlementDate(), 1))) {
->>>>>>> origin/feature/warehouse-v2
+
                     throw new ServiceException("settlement.future");
                 }
             }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/feature/warehouse-v2
             //每个仓库下每个物料在该会计年月之前的余额和余量
 //            Map<Long/*warehouseId*/, AmountAndQuantityDto> eachWarehouseBalance = doctorWarehouseMaterialHandleDao.findEachWarehouseBalanceBySettlementDate(orgId, settlementDate);
             Map<String/*warehouseId-materialId*/, AmountAndQuantityDto> eachWarehouseBalance = doctorWarehouseStockMonthlyDao.findEachWarehouseBalanceBySettlementDate(orgId, DateUtils.addMonths(settlementDate, -1));
@@ -136,13 +130,7 @@ public class DoctorWarehouseSettlementServiceImpl implements DoctorWarehouseSett
             Map<Long/*materialHandleId*/, DoctorWarehouseMaterialHandle> settlementMaterialHandles = new HashMap<>();
             Stopwatch stopwatch = Stopwatch.createStarted();
 
-<<<<<<< HEAD
 
-            Stopwatch stopwatch = Stopwatch.createStarted();
-
-
-=======
->>>>>>> origin/feature/warehouse-v2
             log.info("start to settlement org {} material handle for {}", settlementDate);
             List<DoctorWarehouseMaterialHandle> materialHandles = doctorWarehouseMaterialHandleDao.findByOrgAndSettlementDate(orgId, settlementDate);
 
@@ -253,11 +241,7 @@ public class DoctorWarehouseSettlementServiceImpl implements DoctorWarehouseSett
 //            });
 
             log.info("update or create stock monthly balance under org {} use :{}ms", orgId, stopwatch.elapsed(TimeUnit.MILLISECONDS));
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> origin/feature/warehouse-v2
             if (null == settlement) {
                 settlement = new DoctorWarehouseOrgSettlement();
                 settlement.setOrgId(orgId);
