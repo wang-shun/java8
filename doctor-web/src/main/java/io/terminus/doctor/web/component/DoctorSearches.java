fbaa0aa9f5ab01f57f75bdfe4dbf9720631fa253
liyuhang <liyuhang@kuangjie.net>
2018-07-10 14:17:30 +0800

    :'问题修改部署'

--- doctor-web/src/main/java/io/terminus/doctor/web/component/DoctorSearches.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/component/DoctorSearches.java
@@ -200,7 +200,7 @@ public class DoctorSearches {
             }
             eventCriteria.put("beginDate", params.get("beginDate"));
             eventCriteria.put("endDate", params.get("endDate"));
-            eventCriteria.put("farmId", params.get("farmId"));
+            //eventCriteria.put("farmId", params.get("farmId"));
             eventCriteria = Params.filterNullOrEmpty(eventCriteria);
             List<Long> pigIds = RespHelper.or(doctorPigEventReadService.findPigIdsBy(eventCriteria), null);
             if (CollectionUtils.isEmpty(pigIds)) {
@@ -218,24 +218,14 @@ public class DoctorSearches {
             objectMap.put("statuses", Splitters.splitToInteger(objectMap.get("statuses").toString(), Splitters.UNDERSCORE));
         }
 
-        Paging<SearchedPig> paging = null;
-        if(objectMap.containsKey("statuses") && !((List)objectMap.get("statuses")).contains(0)){
-            if(objectMap.containsKey("statuses")
-                    && ((List)objectMap.get("statuses")).contains(PigStatus.CHG_FARM.getKey())){
-                log.error("pagePigs:"+1+":"+objectMap.toString());
-                paging = RespHelper.or500(doctorPigReadService.pagingChgFarmPig(objectMap, pageNo, pageSize));
-            } else {
-                log.error("pagePigs:"+2+":"+objectMap.toString());
-                paging = RespHelper.or500(doctorPigReadService.pagingPig(objectMap, pageNo, pageSize));
-            }
-        } else{
-            Paging<SearchedPig> paging2 = RespHelper.or500(doctorPigReadService.pagingPig(objectMap, pageNo, pageSize));
-            Paging<SearchedPig> paging1 = RespHelper.or500(doctorPigReadService.pagingChgFarmPig(objectMap, pageNo, pageSize));
-            List<SearchedPig> a = paging1.getData();
-            List<SearchedPig> b =  paging2.getData();
-            a.addAll(b);
-            paging.setData(a);
-            paging.setTotal(paging1.getTotal() + paging2.getTotal());
+        Paging<SearchedPig> paging;
+        if(objectMap.containsKey("statuses")
+                && ((List)objectMap.get("statuses")).contains(PigStatus.CHG_FARM.getKey())){
+            log.error("pagePigs:"+1+":"+objectMap.toString());
+            paging = RespHelper.or500(doctorPigReadService.pagingChgFarmPig(objectMap, pageNo, pageSize));
+        } else {
+            log.error("pagePigs:"+2+":"+objectMap.toString());
+            paging = RespHelper.or500(doctorPigReadService.pagingPig(objectMap, pageNo, pageSize));
         }
         paging.getData().forEach(searchedPig -> {
             if(searchedPig.getPigType() != null){
