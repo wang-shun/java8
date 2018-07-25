e7d8987b6bcd898074b67afce174c775d7b28c5b
jiangshengjin <763232110@qq.com>
2018-07-25 18:57:20 +0800

    jsj

--- doctor-web/src/main/java/io/terminus/doctor/web/front/report/ReportDeliveryController.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/report/ReportDeliveryController.java
@@ -162,4 +162,93 @@ public class ReportDeliveryController {
         return doctorDeliveryReadService.boarReport(farmId,queryDate,pigCode,staffName,barnId,breedId,pigStatus,beginDate,endDate);
 
     }
+
+    //母猪存栏报表导出EXCEL
+    @RequestMapping(method = RequestMethod.GET, value = "sows/export")
+    public void sowsReports(@RequestParam(required = true) Long farmId,
+                                               @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date time,
+                                               @RequestParam(required = false) String pigCode,
+                                               @RequestParam(required = false) String operatorName,
+                                               @RequestParam(required = false) Long barnId,
+                                               @RequestParam(required = false) int breed,
+                                               @RequestParam(required = false) int parity,
+                                               @RequestParam(required = false) int pigStatus,
+                                               @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date inFarmTime,
+                            HttpServletRequest request, HttpServletResponse response) {
+        List<Map<String,Object>> ls=doctorDeliveryReadService.sowsReport(farmId,time,pigCode,operatorName,barnId,breed,parity,pigStatus,inFarmTime);
+
+        //开始导出
+        try  {
+            //导出名称
+            exporter.setHttpServletResponse(request,  response,"母猪存栏报表");
+            try  (XSSFWorkbook workbook  =  new  XSSFWorkbook())  {
+                //表
+                Sheet sheet  =  workbook.createSheet();
+                sheet.addMergedRegion(new CellRangeAddress(0,0,0,20));
+                Row count = sheet.createRow(0);
+//                count.createCell(0).setCellValue("总配种数:"+String.valueOf(map.get("matingcount"))
+//                        +"  分娩数/分娩率:"+String.valueOf(map.get("deliverycount"))+"/"+String.valueOf(map.get("deliveryrate"))
+//                        +"  返情数/返情率:"+String.valueOf(map.get("fqcount"))+"/"+String.valueOf(map.get("fqrate"))
+//                        +"  流产数/流产率:"+String.valueOf(map.get("lccount"))+"/"+String.valueOf(map.get("lcrate"))
+//                        +"  阴性数/阴性率:"+String.valueOf(map.get("yxcount"))+"/"+String.valueOf(map.get("yxrate"))
+//                        +"  死亡数/死亡率:"+String.valueOf(map.get("swcount"))+"/"+String.valueOf(map.get("swrate"))
+//                        +"  淘汰数/淘汰率:"+String.valueOf(map.get("ttcount"))+"/"+String.valueOf(map.get("ttrate"))
+//                );
+
+                Row title  =  sheet.createRow(1);
+                int  pos  =  2;
+
+                title.createCell(0).setCellValue("序号");
+                title.createCell(1).setCellValue("猪舍");
+                title.createCell(2).setCellValue("耳号");
+                title.createCell(3).setCellValue("胎次");
+                title.createCell(4).setCellValue("母猪状态");
+                title.createCell(5).setCellValue("品种");
+                title.createCell(6).setCellValue("来源");
+                title.createCell(7).setCellValue("当前带仔数");
+                title.createCell(8).setCellValue("进场日期");
+                title.createCell(9).setCellValue("出生日期");
+                title.createCell(10).setCellValue("饲养员");
+//                title.createCell(11).setCellValue("预产日期");
+//                title.createCell(12).setCellValue("实产日期");
+//                title.createCell(13).setCellValue("妊娠检查结果");
+//                title.createCell(14).setCellValue("是否死淘");
+
+                for(int i = 0;i<ls.size();i++) {
+                    Map a = ls.get(i);
+                    Row row = sheet.createRow(pos++);
+                    row.createCell(0).setCellValue(String.valueOf(i+1));
+                    row.createCell(1).setCellValue(String.valueOf(a.get("current_barn_name")));
+                    row.createCell(2).setCellValue(String.valueOf(a.get("pig_code")));
+                    row.createCell(3).setCellValue(String.valueOf(a.get("parity")));
+                    row.createCell(4).setCellValue(String.valueOf(a.get("status")));
+                    row.createCell(5).setCellValue(String.valueOf(a.get("breed_name")));
+                    row.createCell(6).setCellValue(String.valueOf(a.get("source")));
+                    row.createCell(7).setCellValue(String.valueOf(a.get("daizaishu")));
+                    row.createCell(8).setCellValue(String.valueOf(a.get("in_farm_date")));
+                    row.createCell(9).setCellValue(String.valueOf(a.get("birth_date")));
+                    row.createCell(10).setCellValue(String.valueOf(a.get("staff_name")));
+//                    row.createCell(11).setCellValue(String.valueOf(a.get("judge_preg_date")));
+//                    row.createCell(12).setCellValue(String.valueOf(a.get("deliveryDate")));
+//                    String check_event_at = "";
+//                    String leave_event_at = "";
+//                    if (!String.valueOf(a.get("check_event_at")).equals("") && !String.valueOf(a.get("notdelivery")).equals("阳性")) {
+//                        check_event_at = "("+String.valueOf(a.get("check_event_at"))+")";
+//                    }
+//                    if (!String.valueOf(a.get("leave_event_at")).equals("")) {
+//                        leave_event_at = "("+String.valueOf(a.get("leave_event_at"))+")";
+//                    }
+//                    row.createCell(13).setCellValue(String.valueOf(a.get("notdelivery"))+check_event_at);
+//                    row.createCell(14).setCellValue(String.valueOf(a.get("deadorescape"))+leave_event_at);
+                }
+                workbook.write(response.getOutputStream());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
