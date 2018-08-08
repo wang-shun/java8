f052452363bbb60366a3cb6b945164e01f14e3c2
liyuhang <liyuhang@kuangjie.net>
2018-08-08 19:36:01 +0800

    :'lyh'

--- doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorPigs.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorPigs.java
@@ -348,17 +348,17 @@ public class DoctorPigs {
                 statusDay = DateUtil.getDeltaDays(chgFarm.getEventAt(), new Date()) + 1;
             } else {
                 String extra = doctorPigTrack.getExtra();
-                Integer status = dto.getDoctorPigTrack().getStatus();
-                Date eventAt = null;
-                Long pigId = dto.getDoctorPig().getId();
-                // 最近一次的配种时间
-                if (status == 4 || status == 7){
-                    eventAt = RespHelper.or500(doctorPigEventReadService.findMateEventToPigId(pigId));
-                } else {
-                    eventAt = RespHelper.or500(doctorPigEventReadService.findEventAtLeadToStatus(pigId, status));
-                }
-//                Date eventAt = RespHelper.or500(doctorPigEventReadService.findEventAtLeadToStatus(dto.getDoctorPig().getId()
-//                        , dto.getDoctorPigTrack().getStatus()));
+//                Integer status = dto.getDoctorPigTrack().getStatus();
+//                Date eventAt = null;
+//                Long pigId = dto.getDoctorPig().getId();
+//                // 最近一次的配种时间
+//                if (status == 4 || status == 7){
+//                    eventAt = RespHelper.or500(doctorPigEventReadService.findMateEventToPigId(pigId));
+//                } else {
+//                    eventAt = RespHelper.or500(doctorPigEventReadService.findEventAtLeadToStatus(pigId, status));
+//                }
+                Date eventAt = RespHelper.or500(doctorPigEventReadService.findEventAtLeadToStatus(dto.getDoctorPig().getId()
+                        , dto.getDoctorPigTrack().getStatus()));
                 statusDay = DateUtil.getDeltaDays(eventAt, new Date()) + 1;
 
                 if (doctorPigTrack.getStatus() == PigStatus.KongHuai.getKey() && StringUtils.isNotBlank(extra)) {
