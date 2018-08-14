8b90c5cb47b4c8fbc87ae0bbc0819fcce40bec7d
kongjingjun <kongjj@fominfo.com>
2018-08-14 14:26:03 +0800

    'a'

--- doctor-event/src/main/java/io/terminus/doctor/event/handler/sow/DoctorSowFarrowingHandler.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/handler/sow/DoctorSowFarrowingHandler.java
@@ -78,7 +78,7 @@ public class DoctorSowFarrowingHandler extends DoctorAbstractEventHandler {
 
 
         //计算分娩日期与配种日期相差天数
-        long between1 = farrowingDate1.getTime()- firstMate.getMattingDate().getTime();
+        long between1 = farrowingDate1.getTime()- firstMate.getEventAt().getTime();
         if (between1 > (2400 * 3600 * 1000)){
             doctorPigEvent.setFarrowingDate(farrowingDate.toDate());
         }else{
