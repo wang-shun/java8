58d26eb895607bf5b924f627bac835bdf7a0d769
kongjingjun <kongjj@fominfo.com>
2018-08-21 18:16:44 +0800

    'a'

--- doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigJoinDao.java
+++ doctor-event/src/main/java/io/terminus/doctor/event/dao/DoctorPigJoinDao.java
@@ -30,8 +30,8 @@ public class DoctorPigJoinDao extends MyBatisDao<SearchedPig> {
         if (params == null) {    //如果查询条件为空
             params = Maps.newHashMap();
         }
-        log.info("pigPagingWithJoin1"+params.toString()+"======="+((Integer[])params.get("statuses"))[0]);
-        if((int)params.get("pigType") == 2 && ((Integer[])params.get("statuses"))[0]==12){
+        log.info("pigPagingWithJoin1"+params.toString()+"======="+((int[])params.get("statuses"))[0]);
+        if((int)params.get("pigType") == 2 && ((int[])params.get("statuses"))[0]==12){
             params.put("isRemoval",1);
             params.remove("statuses");
         }
