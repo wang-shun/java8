bc4dd1c492c9be86bad27045a1717ce4e5827449
kongjingjun <kongjj@fominfo.com>
2018-08-21 21:33:08 +0800

    '测试'

--- doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorBarns.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/event/controller/DoctorBarns.java
@@ -557,7 +557,7 @@ public class DoctorBarns {
         doctorFarmAuthCenter.checkFarmAuth(farmId);
         log.error("========="+params);
         params.put("farmId", farmId);
-        if(params.get("status")==null ||params.get("status")==""){
+        if(params.get("status")==null ||params.get("status").equals("")){
             params.put("status",1);
         }
         return RespHelper.or500(doctorBarnReadService.countForTypes(params));
