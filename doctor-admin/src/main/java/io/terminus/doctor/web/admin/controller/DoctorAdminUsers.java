a105d97e3001aa91a16e2838af7e1aa88cbaa229
kongjingjun <kongjj@fominfo.com>
2018-08-23 22:24:29 +0800

    'a'

--- doctor-admin/src/main/java/io/terminus/doctor/web/admin/controller/DoctorAdminUsers.java
+++ doctor-admin/src/main/java/io/terminus/doctor/web/admin/controller/DoctorAdminUsers.java
@@ -337,7 +337,7 @@ public class DoctorAdminUsers {
      * @param role 运营角色
      * @return 角色主键 ID
      */
-    @RequestMapping(value = "/addRole", method = RequestMethod.POST)
+    @RequestMapping(value = "/addRole", method = RequestMethod.GET)
     public Long createRole(@RequestBody SubRole role) {
         role.setAppKey("PC");
         role.setStatus(1);
