15c416760c980db473d00d029829e3bf7099fae4
kongjingjun <kongjj@fominfo.com>
2018-08-24 12:05:08 +0800

    'a'

--- doctor-web/src/main/java/io/terminus/doctor/web/front/event/impl/DoctorGroupWebServiceImpl.java
+++ doctor-web/src/main/java/io/terminus/doctor/web/front/event/impl/DoctorGroupWebServiceImpl.java
@@ -436,6 +436,9 @@ public class DoctorGroupWebServiceImpl implements DoctorGroupWebService {
                 params.put("toBarnName", getBarnName(getLong(params, "toBarnId")));
                 return new DoctorGroupInputInfo(groupDetail, map(putBasicFields(params), DoctorMoveInGroupInput.class));
             case CHANGE:
+                if(params.get("weight") == null){
+                    params.put("weight",0);
+                }
                 checkParam(params, groupDetail.getGroup().getGroupCode());
                 params.put("changeTypeName", getBasicName(getLong(params, "changeTypeId")));
                 params.put("changeReasonName", getChangeReasonName(getLong(params, "changeReasonId")));
