52d19653deb9a398f7e753d1eadc684688a42b81
yushiqiang <yushiqiang@kuangjie.net>
2018-08-27 09:18:51 +0800

    ''

--- doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseStockHandleWriteServiceImpl.java
+++ doctor-basic/src/main/java/io/terminus/doctor/basic/service/warehouseV2/DoctorWarehouseStockHandleWriteServiceImpl.java
@@ -111,7 +111,7 @@ public class DoctorWarehouseStockHandleWriteServiceImpl implements DoctorWarehou
             if(type == 13){
                 warehouseReturnManager.delete(handle);
                 DoctorWarehouseMaterialApply apply = doctorWarehouseMaterialApplyDao.findMaterialHandle(handle.getRelMaterialHandleId());
-                apply.setQuantity(handle.getQuantity());
+                apply.setRefundQuantity(handle.getQuantity());
                 doctorWarehouseMaterialApplyDao.update(apply);
                 doctorWarehouseStockManager.out(handle.getMaterialId(),handle.getQuantity(),wareHouse);
             }
